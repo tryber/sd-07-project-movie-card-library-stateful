c40a05cbb9f5607c172e3eaa2b8e6963e058d101
Bruna Fonsêca <1998bfonseca@gmail.com>
2020-11-12 19:10:18 -0300

    fixing issues in search componet

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -14,28 +14,40 @@ class SearchBar extends React.Component {
 
     return (
       <form data-testid="search-bar-form">
-        <label 
+        <label
           htmlFor="input-search-text"
-          data-testid="text-input-label">Inclui o texto:
+          data-testid="text-input-label"
+        >Inclui o texto:
         </label>
-        <input id="input-search-text" 
+        <input
+          id="input-search-text"
           type="text"
           value={searchText}
           onChange={onSearchTextChange}
-          data-testid="text-input">
+          data-testid="text-input"
+        >
         </input>
-        <label htmlFor="check-just-favs" data-testid="checkbox-input-label">Mostrar somente favoritos</label>
-        <input type="checkbox" 
-          id="check-just-favs" 
+        <label
+          htmlFor="check-just-favs"
+          data-testid="checkbox-input-label"
+        >Mostrar somente favoritos
+        </label>
+        <input
+          type="checkbox"
+          id="check-just-favs"
           checked={bookmarkedOnly}
           onChange={onBookmarkedChange}
-          data-testid="checkbox-input">
-        </input> 
-        <label htmlFor="genre-selection-movie" 
-          data-testid="select-input-label">
-            Filtrar por gênero
+          data-testid="checkbox-input"
+        >
+        </input>
+        <label 
+          htmlFor="genre-selection-movie"
+          data-testid="select-input-label"
+        >
+        Filtrar por gênero
         </label>
-        <select id="genre-selection-movie" 
+        <select
+          id="genre-selection-movie"
           value={selectedGenre}
           onChange={onSelectedGenreChange}
           data-testid="select-input">
