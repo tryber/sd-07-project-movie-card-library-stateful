219f6a3b8f9ff43ce09006bcfa40749143c455ba
murilopereira006 <muriloedu006@gmail.com>
2020-11-12 18:40:17 -0300

    forms tag e proptypes ok!

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,19 +4,18 @@ import PropTypes from 'prop-types';
 class SearchBar extends React.Component {
   render() {
     return(
-        <form action=""></form>
+        <form data-testid="search-bar-form"></form>
     );
   }
 }
 
-/* SearchBar.propTypes = {
-    searchText: PropTypes.string,
-    searchText, uma string
-    onSearchTextChange, uma callback
-    bookmarkedOnly, um boolean
-    onBookmarkedChange, uma callback
-    selectedGenre, uma string
-    onSelectedGenreChange, uma callback
-}; */
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+};
 
 export default SearchBar;
