085c944c8197576aede4dbce1c8af995ae130b1b
murilopereira006 <muriloedu006@gmail.com>
2020-11-12 19:24:05 -0300

    tudo certo com a SearchBox!

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
 class SearchBar extends React.Component {
-/*  constructor(props) {
+  /*  constructor(props) {
     super(props);
     this.teste = this.teste.bind(this);
     
@@ -26,17 +26,41 @@ class SearchBar extends React.Component {
       onSelectedGenreChange,
     } = this.props;
 
-    return(
-        <form data-testid="search-bar-form">
-            <label htmlFor="searchText" data-testid="text-input-label">
-              <input 
-                type="text"
-                value={searchText}
-                onChange={onSearchTextChange}
-                data-testid="text-input"
-              />
-            </label>
-        </form>
+    return (
+      <form data-testid="search-bar-form">
+        <label htmlFor="searchText" data-testid="text-input-label">
+          Inclui o texto
+          <input
+            type="text"
+            value={searchText}
+            onChange={onSearchTextChange}
+            data-testid="text-input"
+          />
+        </label>
+        <label htmlFor="bookmarked" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            checked={bookmarkedOnly}
+            onChange={onBookmarkedChange}
+            data-testid="checkbox-input"
+          />
+        </label>
+        <label htmlFor="genre" data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+          name="genre"
+          value={selectedGenre}
+          onChange={onSelectedGenreChange}
+          data-testid="select-input"
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
+        </label>
+      </form>
     );
   }
 }
