93f30b45e8fc82501364cdbac0855b532f5cde60
ygorvmdf <ygorvitor.fonseca@gmail.com>
2020-11-12 17:14:51 -0300

    criando componente AddMovie

--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -1,1 +1,16 @@
 // implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+    render() {
+        const { onClick } = this.props;
+
+        return (
+            <form>
+                
+            </form>
+        );
+    }
+}
+
+export default AddMovie;
