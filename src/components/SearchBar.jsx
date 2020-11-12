0c49b0eaefda7e7006b4a9cd1fa691bd4919bd65
Ana Karine <anakarine.ti@gmail.com>
2020-11-12 16:40:22 -0300

    render input-label

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 class SearchBar extends React.Component {
   render() {
+    const { searchText, onSearchTextChange } = this.props;
     return (
-      <form data-testid="search-bar-form"></form>
+      <form data-testid="search-bar-form">
+        <fieldset>
+          <label htmlFor="text" data-testid="text-input-label">Inclui o texto:</label>
+          <input type="text" data-testid="text-input" value={searchText} onChange={onSearchTextChange} />
+        </fieldset>
+      </form>
     );
   }
 }
