547b5f7af41d5c92397118d92685a101ebdfb11e
mustang-roy <lfsmariz@gmail.com>
2020-11-12 15:08:27 -0300

    first commit

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,135 @@
-// implement AddMovie component here
+import React from "react";
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: "",
+      title: "",
+      imagePath: "",
+      storyline: "",
+      rating: 0,
+      genre: "action",
+    };
+    this.handleEventAddMovie = this.handleEventAddMovie.bind(this);
+    this.submitButton = this.submitButton.bind(this);
+  }
+
+  handleEventAddMovie({ target }) {
+    const MovieSpecKey = target.name;
+    const type = target.type;
+    const MovieSpecValue =
+      type === "checkbox"
+        ? target.checked 
+        : type === "number"
+        ? Number(target.value)
+        : target.value;
+
+    this.setState({ [MovieSpecKey]: MovieSpecValue });
+  }
+
+  submitButton(functionCall) {
+    const originState = {
+      subtitle: "",
+      title: "",
+      imagePath: "",
+      storyline: "",
+      rating: 0,
+      genre: "action",
+    };
+
+    functionCall(this.state);
+    this.setState(originState);
+  }
+
+  render() {
+    const { onClick } = this.props;
+
+    return (
+      <form data-testid="add-movie-form" onSubmit={() => {}}>
+        <fieldset>
+          <label data-testid="title-input-label">Título</label>
+          <input
+            name="title"
+            value={this.state.title}
+            data-testid="title-input"
+            onChange={this.handleEventAddMovie}
+            type="text"
+          />
+        </fieldset>
+        <fieldset>
+          <label data-testid="subtitle-input-label">Subtítulo</label>
+          <input
+            name="subtitle"
+            value={this.state.subtitle}
+            data-testid="subtitle-input"
+            onChange={this.handleEventAddMovie}
+            type="text"
+          />
+        </fieldset>
+        <fieldset>
+          <label data-testid="image-input-label">Imagem</label>
+          <input
+            name="imagePath"
+            value={this.state.imagePath}
+            data-testid="image-input"
+            onChange={this.handleEventAddMovie}
+            type="text"
+          />
+        </fieldset>
+        <fieldset>
+          <label data-testid="storyline-input-label">Sinopse</label>
+          <textarea
+            onChange={this.handleEventAddMovie}
+            name="storyline"
+            value={this.state.storyline}
+            cols="30"
+            rows="10"
+            data-testid="storyline-input"
+          />
+        </fieldset>
+        <fieldset>
+          <label data-testid="rating-input-label">Avaliação</label>
+          <input
+            name="rating"
+            value={this.state.rating}
+            data-testid="rating-input"
+            onChange={this.handleEventAddMovie}
+            type="number"
+          />
+        </fieldset>
+        <fieldset>
+          <label data-testid="genre-input-label">Gênero</label>
+          <select
+            name= "genre"
+            value={this.state.genre}
+            onChange={this.handleEventAddMovie}
+            data-testid="genre-input"
+          >
+            <option data-testid="genre-option" value="action">
+              Ação
+            </option>
+            <option data-testid="genre-option" value="comedy">
+              Comédia
+            </option>
+            <option data-testid="genre-option" value="thriller">
+              Suspense
+            </option>
+          </select>
+        </fieldset>
+        <button
+          type="submit"
+          data-testid="send-button"
+          onClick={(event) => {
+            this.submitButton(onClick);
+            event.preventDefault() 
+          }}
+        >
+          Adicionar filme
+        </button>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,62 @@
-// implement SearchBar component here
+import React from "react";
+
+class SearchBar extends React.Component {
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+
+    return (
+      <form data-testid="search-bar-form" action="">
+        <fieldset>
+          <label data-testid="text-input-label">Inclui o texto</label>
+          <input
+            type="text"
+            value={searchText}
+            onChange={onSearchTextChange}
+            data-testid="text-input"
+          />
+        </fieldset>
+        <fieldset>
+          <label data-testid="checkbox-input-label">
+            Mostrar somente favoritos
+          </label>
+          <input
+            type="checkbox"
+            checked={bookmarkedOnly}
+            onChange={onBookmarkedChange}
+            data-testid="checkbox-input"
+          />
+        </fieldset>
+        <fieldset>
+          <label data-testid="select-input-label">Filtrar por gênero</label>
+          <select
+            value={selectedGenre}
+            onChange={onSelectedGenreChange}
+            data-testid="select-input"
+          >
+            <option data-testid="select-option" value="">
+              Todos
+            </option>
+            <option data-testid="select-option" value="action">
+              Ação
+            </option>
+            <option data-testid="select-option" value="comedy">
+              Comédia
+            </option>
+            <option data-testid="select-option" value="thriller">
+              Suspense
+            </option>
+          </select>
+        </fieldset>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
