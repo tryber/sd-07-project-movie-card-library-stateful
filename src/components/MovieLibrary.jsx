b36886d1575c545b5d716dae4f11e8698838ebe5
Emerson Moonlit <emoonlit@pop-os.localdomain>
2020-11-12 15:15:42 -0300

    Initial commit

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,24 @@
-// implement MovieLibrary component here
+import React, { Component } from 'react';
+
+import MovieList from './MovieList.jsx';
+import SearchBar from './SearchBar.jsx';
+import AddMovie from './AddMovie.jsx';
+
+class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar />
+        <MovieList movies={this.props.movies} />
+        <AddMovie />
+      </div>
+    )
+  }
+}
+
+export default MovieLibrary;
