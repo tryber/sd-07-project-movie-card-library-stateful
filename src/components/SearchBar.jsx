81955b5f4c159c5a56133bbee02e8b3a45c107e4
Anderson Santana <anderslash@gmail.com>
2020-11-12 18:56:38 -0300

    Checkbox no SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,17 +3,29 @@ import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
-    const { searchText, onSearchTextChange } = this.props;
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange } = this.props;
     return (
       <div>
         <form data-testid="search-bar-form">
           <label htmlFor="search-text" data-testid="text-input-label">
             Inclui o texto:
             <input
+              name="search-text"
               data-testid="text-input"
               value={searchText}
               onChange={onSearchTextChange}
             />
+          </label>
+          <label htmlFor="bookmarked-change" data-testid="checkbox-input-label">
+            <input
+              type="checkbox"
+              name="bookmarked-change"
+              data-testid="checkbox-input"
+              checked={bookmarkedOnly}
+              onChange={onBookmarkedChange}
+              data-key="bookmarkedOnly"
+            />
+          Mostrar somente favoritos
           </label>
         </form>
       </div>
@@ -24,8 +36,8 @@ class SearchBar extends React.Component {
 SearchBar.propTypes = {
   searchText: PropTypes.string.isRequired,
   onSearchTextChange: PropTypes.func.isRequired,
-//   bookmarkedOnly: PropTypes.bool.isRequired,
-//   onBookmarkedChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
 //   selectedGenre: PropTypes.string.isRequired,
 //   onSelectedGenreChange: PropTypes.func.isRequired,
 };
