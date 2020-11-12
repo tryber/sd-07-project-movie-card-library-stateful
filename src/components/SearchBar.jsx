8f53ee20d030ffe84d6f5d75f7e919c4464711f0
Lenodeoliveira <lenodeoliveira@gmail.com>
2020-11-12 18:11:56 -0300

    Correções codeclimate

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,11 +4,11 @@ class SearchBar extends Component {
   render() {
     return (
       <form data-testid="search-bar-form">
-         <label data-testid="text-input-label" htmlFor="search">
+        <label data-testid="text-input-label" htmlFor="search">
        Inclui o texto
         <input data-testid="text-input" type="text" name="" id="search" />
          </label>
-       </form>);
+      </form>);
   }
 }
 
