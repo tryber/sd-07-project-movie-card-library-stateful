1c7558f6ed14ef3a46c6ba4d1654524405b8fddc
Demistocles <demi.treze@gmail.com>
2020-11-12 19:57:56 -0300

    um moi de requisito

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,14 +6,43 @@ class SearchBar extends React.Component {
     return (
       <div>
         <form data-testid="search-bar-form">
-          <label htmlFor="search-input" data-testid="text-input-label">Inclui o texto</label>
-          <input data-testid="text-input" onChange={onSearchTextChange} value={searchText} id="search-input" type="text"/>
+          <label
+            htmlFor="search-input"
+            data-testid="text-input-label">Inclui o texto</label>
+          <input
+            data-testid="text-input"
+            onChange={onSearchTextChange}
+            value={searchText} id="search-input"
+            type="text"/>
           <label data-testid="checkbox-input-label">Mostrar somente favoritos</label>
-          <input data-testid="checkbox-input" onChange={onBookmarkedChange} checked={bookmarkedOnly} type="checkbox"/>
+          <input
+            data-testid="checkbox-input"
+            onChange={onBookmarkedChange}
+            checked={bookmarkedOnly}
+            type="checkbox"/>
+          <label data-testid="select-input-label">Filtrar por gênero</label>
+          <select
+            data-testid="select-input"
+            onChange={onSelectedGenreChange}
+            value={selectedGenre}
+            checked={bookmarkedOnly}>
+            <option
+              data-testid="select-option"
+              value="">Todos</option>
+            <option
+              data-testid="select-option"
+              value="action">Ação</option>
+            <option
+              data-testid="select-option"
+              value="comedy">Comédia</option>
+            <option
+              data-testid="select-option"
+              value="thriller">Suspense</option>
+          </select>
         </form>
       </div>
     );
   }
-}
+};
 
 export default SearchBar;
