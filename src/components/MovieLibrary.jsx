aa7d6fd30ef5136ecbf2b26f4779da29a6ff0376
gabrieldeori <deori.pessoal@gmail.com>
2020-11-12 16:46:46 -0300

    SearchBar.jsx | Gold - CC + CP

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
 import MovieList from './MovieList';
 
 class MovieLibrary extends React.Component {
-    render() {
-        return (
-            <div>
-                <MovieList movies={this.props.movies}/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <SearchBar
+          searchText={''}
+          onSearchTextChange={() => { }}
+          bookmarkedOnly={false}
+          onBookmarkedChange={() => { }}
+          selectedGenre={''}
+          onSelectedGenreChange={() => { }}
+        />
+        <MovieList movies={this.props.movies} />
+      </div>
+    );
+  }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      subtitle: PropTypes.string.isRequired,
+      storyline: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+      imagePath: PropTypes.string.isRequired,
+      bookmarked: PropTypes.bool.isRequired,
+      genre: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
 
 export default MovieLibrary;
