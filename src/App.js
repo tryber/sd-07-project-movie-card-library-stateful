1392d1ef9ea25f46763c30db110b612c842605fe
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2020-11-12 16:34:18 -0300

    Iniciando AddMovie

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
-
+import AddMovie from './components/AddMovie';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <AddMovie />
     </div>
   );
 }
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 class AddMovie extends Component {
-  render() {
+    constructor(){
+        super()
+        this.handleState = this.handleState.bind(this);
+        this.state = {
+            subtitle: '',
+            title: '',
+            imagePath: '',
+            storyline: '',
+            rating: 0,
+            genre: 'action'
+        }
+    }
+
+    handleState(value){
+        this.setState({title: value});
+    }
+    render() {
+        const { onclick } = this.props;
     return (
       <div>
         <form>
-            
+            <label data-testid="title-input-label" >Título</label>
+            <input type='text' value={this.state.title} data-testid="title-input" onChange={this.handleState}></input>
         </form>
       </div>
     );
