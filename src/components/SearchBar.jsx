4610e2b4247b9e34cbb45d659bf76576edfadaad
Bruna Fonsêca <1998bfonseca@gmail.com>
2020-11-12 15:34:15 -0300

    requisition 4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,6 +8,8 @@ class SearchBar extends React.Component {
       <form data-testid="search-bar-form">
         <label for="input-search-text" data-testid="text-input-label">Inclui o texto:</label>
         <input id="input-search-text" type="text" value={searchText} onChange={onSearchTextChange} data-testid="text-input"></input>
+        <label for="check-just-favs" data-testid="checkbox-input-label">Mostrar somente favoritos</label>
+        <input type="checkbox" id="check-just-favs" checked={bookmarkedOnly} onChange={onBookmarkedChange} data-testid="checkbox-input"></input> 
       </form>
     )
   }
