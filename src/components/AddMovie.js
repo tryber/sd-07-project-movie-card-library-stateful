c39572fe6ca21b349e163af14ccde7e998596f01
ygorvmdf <ygorvitor.fonseca@gmail.com>
2020-11-12 17:52:37 -0300

    criando input title dentro do forms do AddMovie

--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -2,12 +2,32 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            subtitle: '',
+            title: '',
+            imagePath: '',
+            storyline: '',
+            rating: 0,
+            genre: 'action'
+        }
+    }
+
+    addTitle = ({target}) => {
+        this.setState({
+            title: target.value
+        })
+    }
+
     render() {
         const { onClick } = this.props;
 
         return (
-            <form>
-                
+            <form data-testid="add-movie-form">
+                <label data-testid="title-input-label">Título</label>
+                <input onChange={this.addTitle} data-testid="title-input" value={this.state.title} />
             </form>
         );
     }
