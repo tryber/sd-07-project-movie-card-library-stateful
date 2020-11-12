cd5981e7aa1258ffe759f5d39109d3266413bc11
Strongreen <strongreen@live.com>
2020-11-12 15:14:30 -0300

    Iniciando projeto motivada por Pablo Vittar

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,12 @@
-// implement MovieLibrary component here
+import React from 'react';
+
+class MovieLibrary extends React.Component {
+  render() {
+    return (
+        <div>
+            Barra de pesquisa
+        </div>);
+  }
+}
+
+export default MovieLibrary;
