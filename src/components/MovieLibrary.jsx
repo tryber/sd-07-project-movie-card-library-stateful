3ac0ee70210216c54183ab3a60dda11315d63c02
gabrieldeori <deori.pessoal@gmail.com>
2020-11-12 15:15:54 -0300

    Implemented MovieLibrary in App.js and MovieList in MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,14 @@
-// implement MovieLibrary component here
+import React from 'react';
+import MovieList from './MovieList';
+
+class MovieLibrary extends React.Component {
+    render() {
+        return (
+            <div>
+                <MovieList movies={this.props.movies}/>
+            </div>
+        )
+    }
+}
+
+export default MovieLibrary;
