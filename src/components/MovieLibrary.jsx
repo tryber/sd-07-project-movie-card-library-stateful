debe36d9bae10b1c3b725319f199e1f615ea0833
Vanessa Oliveira <vanessanaara@gmail.com>
2020-11-12 15:03:28 -0300

    Iniciando o projeto

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,17 @@
-// implement MovieLibrary component here
+import React from 'react';
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div className='page-title movie-card-header'>
+        <h1>Let's get started!</h1>
+      </div>
+    )
+  }
+}
+
+export default MovieLibrary;
