24dbbc476041d7632be0f8a71239b67f7e96c949
Fernando <nando.neves@gmail.com>
2020-11-12 16:08:39 -0300

    first

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
-
+import SearchBar from './src/components/SearchBar'
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar />
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,34 @@
 // implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+     <div>
+       <form>
+         <label>
+           <input type='text' value={this.props.searchText} onChange={this.props.onSearchTextChange}/>
+         </label>
+         <label>
+           <input type='checklist' name='bookmarkedOnly' onChange={this.props.onBookmarkedChange} />
+         </label>
+         <select onChange={this.props.onSelectedGenreChange}>
+           <option name='masculino'>Masculino</option>
+           <option name='feminino'>Feminino</option>
+         </select>
+         <
+        
+       </form>
+
+  {/* - `searchText`, uma string
+  - `onSearchTextChange`, uma callback
+  - `bookmarkedOnly`, um boolean
+  - `onBookmarkedChange`, uma callback
+  - `selectedGenre`, uma string
+  - `onSelectedGenreChange`, uma callback */}
+     </div>
+    );
+  }
+}
+
+export default SearchBar;
