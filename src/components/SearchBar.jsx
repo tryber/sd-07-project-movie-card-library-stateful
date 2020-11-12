ada010c2b17772b659226781f3bf03e55f563d51
Ana Karine <anakarine.ti@gmail.com>
2020-11-12 16:59:37 -0300

    render checkbox

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 
 class SearchBar extends React.Component {
   render() {
-    const { searchText, onSearchTextChange } = this.props;
+    const { searchText, onSearchTextChange, 
+      bookmarkedOnly, onBookmarkedChange } = this.props;
     return (
       <form data-testid="search-bar-form">
+
         <fieldset>
           <label htmlFor="text" data-testid="text-input-label">Inclui o texto:</label>
           <input type="text" data-testid="text-input" value={searchText} onChange={onSearchTextChange} />
         </fieldset>
+
+        <fieldset>
+          <label htmlFor="checkbox" data-testid="checkbox-input-label">Mostrar somente favoritos</label>
+          <input type="checkbox" data-testid="checkbox-input" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
+        </fieldset>
+
       </form>
     );
   }
