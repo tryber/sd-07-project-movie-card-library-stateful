42800c85733238b0f2d27cc92aec3baf58215287
Jr3564 <rodolfojr94@gmail.com>
2020-11-12 15:55:28 -0300

    Test passando no CC

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -5,16 +5,17 @@ import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
-  constructor(props) {
+  /* constructor(props) {
     super(props);
-  }
+  } */
 
   render() {
+    const prop = this.props;
     return (
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar />
-        <MovieList movies={this.props.movies} />
+        <MovieList movies={prop.movies} />
         <AddMovie />
       </div>
     );
