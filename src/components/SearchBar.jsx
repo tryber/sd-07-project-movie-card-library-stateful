01d669aaf8118435ea3283ded921849eda3de482
Ana Karine <anakarine.ti@gmail.com>
2020-11-12 17:20:55 -0300

    render select

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 class SearchBar extends React.Component {
   render() {
     const { searchText, onSearchTextChange, 
-      bookmarkedOnly, onBookmarkedChange } = this.props;
+      bookmarkedOnly, onBookmarkedChange,
+      selectedGenre, onSelectedGenreChange } = this.props;
     return (
       <form data-testid="search-bar-form">
 
@@ -15,6 +16,16 @@ class SearchBar extends React.Component {
         <fieldset>
           <label htmlFor="checkbox" data-testid="checkbox-input-label">Mostrar somente favoritos</label>
           <input type="checkbox" data-testid="checkbox-input" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
+        </fieldset>
+
+        <fieldset>
+          <label htmlFor="select" data-testid="select-input-label">Filtrar por gênero</label>
+          <select type="select" data-testid="select-input" value={selectedGenre} onChange={onSelectedGenreChange}>
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
         </fieldset>
 
       </form>
