09559fd764d4660dfa89480f8a5fca0d791a0e4f
alvaro <alvarovascon2@gmail.com>
2020-11-12 15:39:54 -0300

    estrutura search completa

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -9,20 +9,35 @@ class SearchBar extends React.Component {
           id="text-input"
           type="text" value={searchText}
           onChange={onSearchTextChange}
-        data-testid="text-input" />
-
+          data-testid="text-input"
+        />
         <input
           id="checkbox-input"
           data-testid="checkbox-input"
           type="checkbox"
           checked={bookmarkedOnly}
-        onChange={onBookmarkedChange} />
+          onChange={onBookmarkedChange}
+        />
         <label
           htmlFor="checkbox-input"
           data-testid="checkbox-input-label" >
             Mostrar somente favoritos
         </label>
-
+        <label
+          htmlFor="select-input"
+          data-testid="select-input-label" >
+            Filtrar por gênero
+        </label>
+        <select
+          id="select-input"
+          data-testid="select-input"
+          value={selectedGenre}
+          onChange={onSelectedGenreChange}>
+          <option data-testid="select-option" value="">Todos</option>
+          <option data-testid="select-option" value="action">Ação</option>
+          <option data-testid="select-option" value="comedy">Comédia</option>
+          <option data-testid="select-option" value="thruller">Suspense</option>
+        </select>
 
       </form>
     );
