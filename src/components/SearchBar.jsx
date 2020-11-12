570ddb4428e061e5f62fad32743e54997ddb252f
brendalima <brenda.c.lima@poli.ufrj.br>
2020-11-12 17:43:25 -0300

    attempt to fix CC issues

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,16 +1,26 @@
 // implement SearchBar component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
 
   render() {
     return (
       <form data-testid="search-bar-form">
-        <label name="text" data-testid="text-input-label">"Inclui o texto:"</label>
-        <input type="text" data-testid="text-input" name="text" value={this.props.searchText} onChange={this.props.onSearchTextChange}/>
+        <label htmlFor="text" data-testid="text-input-label">Inclui o texto:</label>
+        <input type="text" 
+        data-testid="text-input" 
+        id="text" 
+        value={this.props.searchText} 
+        onChange={this.props.onSearchTextChange} />
       </form>
     );
   }
 }
 
 export default SearchBar;
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired
+};
