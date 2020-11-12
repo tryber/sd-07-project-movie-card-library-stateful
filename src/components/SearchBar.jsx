cc0e79836bf295ac68c50418e8fd090ca0f997ef
murilopereira006 <muriloedu006@gmail.com>
2020-11-12 18:56:06 -0300

    ok, deu certo eu so n sei pq kakkak

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
-  constructor(props) {
+/*  constructor(props) {
     super(props);
     this.teste = this.teste.bind(this);
     
@@ -14,18 +14,25 @@ class SearchBar extends React.Component {
       selectedGenre: '',
       onSelectedGenreChange: '',
     };
-  }
+  } */
 
-  teste = () =>{  };
-  
   render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+
     return(
         <form data-testid="search-bar-form">
             <label htmlFor="searchText" data-testid="text-input-label">
               <input 
                 type="text"
-                value={this.prop.searchText}
-                onChange={this.prop.onSearchTextChange}
+                value={searchText}
+                onChange={onSearchTextChange}
                 data-testid="text-input"
               />
             </label>
