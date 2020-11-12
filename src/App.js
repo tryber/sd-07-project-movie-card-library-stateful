2e1102e6a874524def1124aaad09b738e42e8492
Renan Oliveira <oliveira.renan.rg@gmail.com>
2020-11-12 16:26:02 -0300

    Começando a criação do componente MovieLibrary

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import SearchBar from './components/SearchBar';
-import AddMovie from './components/AddMovie';
 import './App.css';
 
 import Header from './components/Header';
@@ -9,8 +7,6 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <SearchBar />
-      <AddMovie />
     </div>
   );
 }
---
+++ src/components/MovieLibrary.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.filterMovies = this.filterMovies.bind(this);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: this.props.movies,
+    };
+  }
+
+  filterMovies() {
+
+  }
+
+  render() {
+    const {
+      searchText,
+      bookmarkedOnly,
+      selectedGenre,
+      movies,
+    } = this.state;
+    return (
+      <div>
+        <SearchBar
+          searchText={searchText}
+          onSearchTextChange={this.filterMovies}
+          bookmarkedOnly={bookmarkedOnly}
+          onBookmarkedChange={this.filterMovies}
+          selectedGenre={selectedGenre}
+          onSelectedGenreChange={this.filterMovies}
+        />
+        <MovieList movies={movies} />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = { movies: PropTypes.arrayOf(PropTypes.object).isRequired };
+
+export default MovieLibrary;
