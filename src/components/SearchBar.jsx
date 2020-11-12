fb165c7c200d8b6240dc87ae2678f6d2f576b311
Anderson Santana <anderslash@gmail.com>
2020-11-12 18:43:44 -0300

    Mais ajustes para o CodeClimate

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,16 +3,17 @@ import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
+    const { searchText, onSearchTextChange } = this.props;
     return (
       <div>
         <form data-testid="search-bar-form">
           <label htmlFor="search-text" data-testid="text-input-label">
             Inclui o texto:
-              <input
-                data-testid="text-input"
-                value={this.props.searchText}
-                onChange={this.props.onSearchTextChange}
-              />
+            <input
+              data-testid="text-input"
+              value={searchText}
+              onChange={onSearchTextChange}
+            />
           </label>
         </form>
       </div>
@@ -23,10 +24,10 @@ class SearchBar extends React.Component {
 SearchBar.propTypes = {
   searchText: PropTypes.string.isRequired,
   onSearchTextChange: PropTypes.func.isRequired,
-  bookmarkedOnly: PropTypes.bool.isRequired,
-  onBookmarkedChange: PropTypes.func.isRequired,
-  selectedGenre: PropTypes.string.isRequired,
-  onSelectedGenreChange: PropTypes.func.isRequired,
+//   bookmarkedOnly: PropTypes.bool.isRequired,
+//   onBookmarkedChange: PropTypes.func.isRequired,
+//   selectedGenre: PropTypes.string.isRequired,
+//   onSelectedGenreChange: PropTypes.func.isRequired,
 };
 
 export default SearchBar;
