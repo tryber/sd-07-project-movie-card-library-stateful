8b6c9e5cd28a1d78e1ecc8bce86538956c1f68eb
Felipe Nascimento <flpnascto@gmail.com>
2020-11-12 16:18:56 -0300

    Criando componente SearchBar.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,13 @@
 // implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+     <div>Search Bar</div> 
+    )
+
+  }
+}
+
+export default SearchBar;
