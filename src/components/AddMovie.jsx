42bbcb0c5d9a26605d215fea6c65999e39a7a780
Felipe Nascimento <flpnascto@gmail.com>
2020-11-12 17:49:46 -0300

    Adicionando um component AddMovie com um formulario vazio.

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,14 @@
 // implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  render() {
+    return (
+      <div>
+        <form data-testid="add-movie-form"></form>
+      </div>
+    );
+  }
+}
+
+export default AddMovie;
