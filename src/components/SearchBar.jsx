1233a4bb72e0be27cbbadb08057bd1aa5d79b217
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2020-11-12 14:49:59 -0300

    Iniciando o projeto. VAMOS COM TUDO 🚀

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,36 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+    render() {
+        return (
+            <div>
+                <form data-testid="search-bar-form">
+                    <label htmlFor='searchText' data-testid="text-input-label">Inclui o texto:</label>
+                    <input 
+                        type='text' 
+                        name='searchText' 
+                        value='searchText' 
+                        onChange={onSearchTextChange} 
+                        data-testid="text-input"
+                    ></input>
+                    <label htmlFor='onlyFavorites' data-testid="checkbox-input-label" >Mostrar somente favoritos</label>
+                    <input 
+                        name='onlyFavorites'
+                        type='checkbox' 
+                        value='bookmarkedOnly' 
+                        onChange={onBookmarkedChange}
+                        data-testid="checkbox-input"
+                    ></input>
+                    <label htmlFor='filter' data-testid="select-input-label">Filtrar por gênero</label>
+                    <select value='selectedGenre' onChange='onSelectedGenreChange' data-testid="select-input">
+                        <option data-testid="select-option" value=''>Todos</option>
+                        <option data-testid="select-option" value='action'>Ação</option>
+                        <option data-testid="select-option" value='comedy'>Comédia</option>
+                        <option data-testid="select-option" value='thriller'>Suspense</option>
+                    </select>
+
+                </form>
+            </div>
+        )
+    }
+}
