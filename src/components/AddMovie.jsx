da943f58d344f8d47f0729523be5fd5d6e73b50f
Bruna Fonsêca <1998bfonseca@gmail.com>
2020-11-12 16:50:26 -0300

    requisition 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,40 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super()
+
+    this.changeTextInput = this.changeTextInput.bind(this);
+
+      this.state = {
+        subtitle: '',
+        title: '',
+        imagePath: '',
+        storyline: '',
+        rating: 0,
+        genre:'action',
+      }
+  }
+
+  changeTextInput(event) {
+    this.setState({ title: event.target.value })
+  }
+ 
+  render() {
+    const { onClick } = this.props;
+    
+    return (
+      <form>
+        <label htmlFor="title-input" data-testid="title-input-label">
+          Título
+        <input type="text" id="title-input" 
+        data-testid="title-input" 
+        value={this.state.title} 
+        onChange={this.changeTextInput}></input>
+        </label>
+      </form>
+    )
+  }
+}
+
+export default AddMovie;
