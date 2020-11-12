691f96e525e5c98f4a4caf4e1a93053547917d93
ygorvmdf <ygorvitor.fonseca@gmail.com>
2020-11-12 16:56:55 -0300

    criando tag select dentro do form

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,6 +9,15 @@ class SearchBar extends React.Component {
                 <form data-testid="search-bar-form">
                     <label data-testid="text-input-label" htmlFor="search-input">Inclui o texto</label>
                     <input data-testid="text-input" onChange={onSearchTextChange} value={searchText} id="search-input" type="text" />
+                    <label data-testid="checkbox-input-label" htmlFor="favorits-input">Mostrar somente favoritos</label>
+                    <input data-testid="checkbox-input" onChange={onBookmarkedChange} type="checkbox" id="favorits-input" checked={bookmarkedOnly} />
+                    <label data-testid="select-input-label" htmlFor="select-gender" value={selectedGenre}>Filtrar por gênero</label>
+                    <select data-testid="select-input" onChange={onSelectedGenreChange} value={selectedGenre} id="select-gender">
+                        <option data-testid="select-option" value="">Todos</option>
+                        <option data-testid="select-option" value="action">Ação</option>
+                        <option data-testid="select-option" value="comedy">Comédia</option>
+                        <option data-testid="select-option" value="thriller">Suspense</option>
+                    </select>
                 </form>
             </div>
         );
