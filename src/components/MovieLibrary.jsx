f01f0d0ce484a36ba16796f8bb584a750dae1593
alvaro <alvarovascon2@gmail.com>
2020-11-12 15:18:33 -0300

    movie cards com react

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,19 @@
-// implement MovieLibrary component here
+import React from 'react';
+
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends React.Component {
+  render() {
+    return(
+      <div>
+        <SearchBar />
+        <MovieList />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,18 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return(
+      <form data-testid="search-bar-form">
+        <label htmlFor="text-input" data-testid="text-input-label">Inclui o texto:</label>
+        <input id="text-input" type ="text" value={searchText} onChange={onSearchTextChange}
+        data-testid="text-input" />
+
+        <input data-testid="checkbox-input" type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange}/>
+        <label data-testid="checkbox-input-label" >Mostrar somente favoritos</label>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
