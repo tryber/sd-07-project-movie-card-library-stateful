ab089a8bc8bfd71390fbe7d29a1fc43c72752078
murilopereira006 <muriloedu006@gmail.com>
2020-11-12 19:43:35 -0300

    apanhando do CC

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
@@ -11,8 +12,6 @@ class Rating extends React.Component {
   }
 }
 
-Rating.propTypes = {
-  rating: PropTypes.number.isRequired
-}
+Rating.propTypes = { rating: PropTypes.number.isRequired }
 
 export default Rating;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -13,38 +13,38 @@ class SearchBar extends React.Component {
       onSelectedGenreChange,
     } = this.props;
 
-  return (
-    <form data-testid="search-bar-form">
-      <label htmlFor="searchText" data-testid="text-input-label">
-        Inclui o texto
-        <input
-          type="text"
-          value={searchText}
-          onChange={onSearchTextChange}
-          data-testid="text-input"
-        />
-        </label>
-        <label htmlFor="bookmarked" data-testid="checkbox-input-label">
-          Mostrar somente favoritos
+    return (
+      <form data-testid="search-bar-form">
+        <label htmlFor="searchText" data-testid="text-input-label">
+          Inclui o texto
           <input
-            type="checkbox"
-            checked={bookmarkedOnly}
-            onChange={onBookmarkedChange}
-            data-testid="checkbox-input"
+            type="text"
+            value={searchText}
+            onChange={onSearchTextChange}
+            data-testid="text-input"
           />
-        </label>
-        <label htmlFor="genre" data-testid="select-input-label">
-          Filtrar por gênero
-          <select
-          name="genre"
-          value={selectedGenre}
-          onChange={onSelectedGenreChange}
-          data-testid="select-input"
-          >
-            <option value="" data-testid="select-option">Todos</option>
-            <option value="action" data-testid="select-option">Ação</option>
-            <option value="comedy" data-testid="select-option">Comédia</option>
-            <option value="thriller" data-testid="select-option">Suspense</option>
+          </label>
+          <label htmlFor="bookmarked" data-testid="checkbox-input-label">
+            Mostrar somente favoritos
+            <input
+              type="checkbox"
+              checked={bookmarkedOnly}
+              onChange={onBookmarkedChange}
+              data-testid="checkbox-input"
+            />
+          </label>
+          <label htmlFor="genre" data-testid="select-input-label">
+            Filtrar por gênero
+            <select
+              name="genre"
+              value={selectedGenre}
+              onChange={onSelectedGenreChange}
+              data-testid="select-input"
+            >
+              <option value="" data-testid="select-option">Todos</option>
+              <option value="action" data-testid="select-option">Ação</option>
+              <option value="comedy" data-testid="select-option">Comédia</option>
+              <option value="thriller" data-testid="select-option">Suspense</option>
           </select>
         </label>
       </form>
