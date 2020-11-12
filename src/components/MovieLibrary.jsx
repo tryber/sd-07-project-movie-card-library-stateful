021381de79473e3e301829947363ee91798e0795
Vanessa Oliveira <vanessanaara@gmail.com>
2020-11-12 16:57:41 -0300

    Estruturando e renderizando o SearchBar

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
+import SearchBar from './SearchBar';
 
 class MovieLibrary extends React.Component {
-  constructor(props) {
-    super(props)
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.onSearchTextChange = this.onSearchTextChange.bind(this);
+  //   this.bookmarkedOnly = this.bookmarkedOnly.bind(this);
+  //   this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+
+  //   this.state = {
+  //     searchText: '',
+  //     bookmarkedOnly: false,
+  //     selectedGenre: '',
+  //   }
+  // }
+
+  // onSearchTextChange() {
+
+  // }
+
+  // onBookmarkedChange() {
+ 
+  // }
+
+  // onSelectedGenreChange() {
+  // }
 
   render() {
     return (
-      <div className='page-title movie-card-header'>
-        <h1>Let's get started!</h1>
+      <div>
+        <SearchBar />
       </div>
-    )
+    );
   }
 }
 
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,56 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+
+    return (
+      <div>
+        <form data-testid="search-bar-form">
+          <label htmlFor="text-include" data-testid="text-input-label">
+            Inclui o texto:
+          </label>
+          <input
+            type="text"
+            value={searchText}
+            onChange={onSearchTextChange}
+            data-testid="text-input"
+          />
+          <br />
+
+          <label htmlFor="favorite" data-testid="checkbox-input-label">
+            Mostrar somente favoritos
+          </label>
+          <input
+            type="checkbox"
+            checked={bookmarkedOnly}
+            onChange={onBookmarkedChange}
+            data-testid="checkbox-input"
+          />
+          <br />
+
+          <label htmlFor="genre" data-testid="select-input-label">Filtrar por gênero</label>
+          <select data-testid="select-input" value={selectedGenre} onChange={onSelectedGenreChange}>
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
