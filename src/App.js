4fa498a014782b5df2d89ff077d3e298099d3537
Fernando Antunes <fernando.antunes1@gmail.com>
2020-11-12 15:34:10 -0300

    Crie um componente chamado SearchBar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
-
 import Header from './components/Header';
+import MovieLibrary from './components/MovieLibrary'
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <MovieLibrary />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,13 @@
 // implement MovieLibrary component here
+import React from 'react';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends React.Component {
+  render() {
+    return(
+      <SearchBar />
+    )
+  }
+}
+
+export default MovieLibrary;
