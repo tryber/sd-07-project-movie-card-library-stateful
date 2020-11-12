4a648b721fef35895d212d652d14cfacc4db8d1a
Luise Rios <luisemiranda@gmail.com>
2020-11-12 20:05:46 -0300

    Iniciando o projeto

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,76 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+  
+  this.onClick = this.onClick.bind(this);
+  this.onSendButton= this.onSendButton.bind(this);
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+
+  }
+
+  onClick (event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  onSendButton = event => {
+    this.onClick(event)
+  }
+
+  render () {
+    return (
+      <form>
+
+        <label data-testid="title-input-label">
+          Título
+          <input type="text" value={this.state.title} onChange={this.onClick} data-testid="title-input"/>
+        </label>
+
+        <label data-testid="subtitle-input-label">
+          Subtítulo
+          <input type="text" value={this.state.subtitle} onChange={this.onClick} data-testid="subtitle-input"/>
+        </label>
+
+        <label data-testid="storyline-input-label">
+          Sinopse
+          <textarea value={this.state.storyline} onChange={this.onClick} data-testid="stpryline-input"> 
+          </textarea>
+        </label>
+
+        <label data-testid="rating-input-label">
+          Avaliação
+          <input type="number" value={this.state.rating} onChange={this.onClick} data-testid="rating-input"/>
+        </label>
+
+        <label data-testid="genre-input-label">
+          Gênero
+          <select value={this.state.genre} onChange={this.onClick} data-testid="genre-input">
+            <option value="action" data-testid="genre-option">Ação</option>
+            <option value="comedy" data-testid="genre-option">Comédia</option>
+            <option value="thriller" data-testid="genre-option">Suspense</option>
+          </select>
+        </label>
+
+        <button data-testid="send-button" onClick={this.onSendButton}>Adicionar filme</button>
+
+      </form>
+    )
+  }
+
+}
+export default AddMovie
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,72 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+    };
+  }
+
+  onSearchTextChange(event) {
+    this.setState({ searchText: event.target.value });
+  }
+
+  onBookmarkedChange(event) {
+    this.setState({ bookmarkedOnly: event.targed.checked });
+  }
+
+  onSelectedGenreChange(event) {
+    this.setState({ selectedGenre: event.target.value });
+  }
+  render() {
+    return (
+      <form data-testid="search-bar-form">
+
+        <label data-testid="text-input-label" htmlFor="text-imput">
+          Inclui o texto:
+          <input
+            type="text"
+            value={this.state.searchText}
+            onChange={this.onSearchTextChange}
+            data-testid="text-input"
+          />
+        </label>
+
+        <label data-testid="checkbox-input-label" htmlFor="checkbox-input">
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            checked={this.state.bookmarkedOnly}
+            onChange={this.onBookmarkedChange}
+            data-testid="checkbox-input"
+          />
+        </label >
+
+        <label data-testid="select-input-label" htmlFor="select-input">
+          Filtrar por gênero
+          <select
+            value={this.state.selectedGenre}
+            onChange={this.onSelectedGenreChange}
+            data-testid="select-input"
+          >
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
+        </label>
+
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
