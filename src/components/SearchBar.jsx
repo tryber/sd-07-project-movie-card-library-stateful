e369eda7514887fa28fe0a31246a260ede561aea
Felipe Nascimento <flpnascto@gmail.com>
2020-11-12 17:29:26 -0300

    Adicionando um checkbox ao componente SearchBar.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -12,10 +12,11 @@ class SearchBar extends React.Component {
       selectedGenre,
       onSelectedGenreChange,
     } = this.props;
+
     return (
       <div>
         <form data-testid="search-bar-form">
-          <label data-testid="text-input-label">
+          <label data-testid="text-input-label" htmlFor="text-input">
             Inclui o texto:
             <input
               data-testid="text-input"
@@ -23,6 +24,17 @@ class SearchBar extends React.Component {
               type="text"
               value={searchText}
               onChange={onSearchTextChange}
+            />
+          </label>
+
+          <label data-testid="checkbox-input-label" htmlFor="checkbox-input">
+            Mostrar somente favoritos
+            <input
+              data-testid="checkbox-input"
+              id="checkbox-input"
+              type="checkbox"
+              checked={bookmarkedOnly}
+              onChange={onBookmarkedChange}
             />
           </label>
         </form>
