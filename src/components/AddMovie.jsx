03e989d72dec67e365a8fb2684034d85fc3312c2
Julio Kauer <juliokauer@gmail.com>
2020-11-12 19:01:43 -0300

    AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,35 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    const { onClick } = this.props;
+    return (
+      <form>
+        <label htmlFor="search-title" data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            name="search-title"
+            value={this.pros.title}
+            // onClick={onClick}
+            data-testid="title-inpu"
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
