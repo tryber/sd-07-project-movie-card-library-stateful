ee60255f8dd88c4d84eaa93e321c110d61b041ba
gabrieldeori <deori.pessoal@gmail.com>
2020-11-12 23:46:13 -0300

    MovieLibrary 75%

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -5,18 +5,43 @@ import MovieList from './MovieList';
 import AddMovie from './AddMovie';
 
 class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: "",
+      bookmarkedOnly: false,
+      selectedGenre: "",
+      movies: this.props.movies,
+    };
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+  }
+
+  onSearchTextChange(event) {
+    this.setState({
+      searchText: event.target.value,
+    });
+  }
+
+  onBookmarkedChange() {
+    console.log('teste2');
+  }
+
+  onSelectedGenreChange() {
+    console.log('teste3');
+  }
+  
   render() {
     return (
       <div>
         <SearchBar
-          searchText={''}
-          onSearchTextChange={() => { }}
+          searchText={this.state.searchText}
+          onSearchTextChange={this.onSearchTextChange}
           bookmarkedOnly={false}
-          onBookmarkedChange={() => { }}
+          onBookmarkedChange={this.onBookmarkedChange}
           selectedGenre={''}
-          onSelectedGenreChange={() => { }}
+          onSelectedGenreChange={this.onSelectedGenreChange}
         />
-        <MovieList movies={this.props.movies} />
+        <MovieList movies={this.state.movies} />
         <AddMovie onClick={() => { }} />
       </div>
     );
