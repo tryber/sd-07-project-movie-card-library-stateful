58aaf41d9d72fcceb400238863358b659545c4da
Mateus Leite Alves de Almeida <mateuslaalmeida@gmail.com>
2020-11-13 20:11:02 -0300

    Alteraçoes para o primeiro requisito

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import MovieCard from './MovieCard';
 
@@ -13,5 +14,7 @@ class MovieList extends React.Component {
     );
   }
 }
+
+MovieList.propTypes = { movies: PropTypes.arrayOf(PropTypes.object).isRequired };
 
 export default MovieList;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,26 @@
-// implement SearchBar component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  render() {
+/*     const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange } = this.props; */
+    return {};
+  }
+}
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.function.isRequired,
+  bookmarkedOnly: PropTypes.boolean.isRequired,
+  onBookmarkedChange: PropTypes.function.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.function.isRequired,
+};
+
+export default SearchBar;
