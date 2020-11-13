615395ae4f2fb7a4000c4e116b3da831871f3dee
gabrieldeori <deori.pessoal@gmail.com>
2020-11-13 13:01:08 -0300

    Everything is GOLD, but MovieLibrary not adding movies to state

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -8,40 +8,53 @@ class MovieLibrary extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchText: "",
+      searchText: '',
       bookmarkedOnly: false,
-      selectedGenre: "",
+      selectedGenre: '',
       movies: this.props.movies,
     };
     this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+    this.moviesFilter = this.moviesFilter.bind(this);
   }
 
   onSearchTextChange(event) {
-    this.setState({
-      searchText: event.target.value,
-    });
+    this.setState({ searchText: event.target.value }, this.moviesFilter);
   }
 
-  onBookmarkedChange() {
-    console.log('teste2');
+  onBookmarkedChange(event) {
+    this.setState({ bookmarkedOnly: event.target.checked });
   }
 
-  onSelectedGenreChange() {
-    console.log('teste3');
+  onSelectedGenreChange(event) {
+    this.setState({ selectedGenre: event.target.value });
   }
-  
+
+  moviesFilter() {
+    let filteredMovies = this.state.movies.filter((movie) =>
+      movie.title.toLowerCase().includes(this.state.searchText.toLowerCase()) ||
+      movie.subtitle.toLowerCase().includes(this.state.searchText.toLowerCase()) ||
+      movie.storyline.toLowerCase().includes(this.state.searchText.toLowerCase()))
+      .filter((movie) => movie.genre.includes(this.state.selectedGenre));
+    if (this.state.bookmarkedOnly) {
+      filteredMovies = filteredMovies.filter((movie) => movie.bookmarked);
+    }
+    return filteredMovies;
+  }
+
   render() {
     return (
       <div>
         <SearchBar
           searchText={this.state.searchText}
           onSearchTextChange={this.onSearchTextChange}
-          bookmarkedOnly={false}
+          bookmarkedOnly={this.state.bookmarkedOnly}
           onBookmarkedChange={this.onBookmarkedChange}
-          selectedGenre={''}
+          selectedGenre={this.state.selectedGenre}
           onSelectedGenreChange={this.onSelectedGenreChange}
         />
-        <MovieList movies={this.state.movies} />
+        <MovieList movies={this.moviesFilter()} />
         <AddMovie onClick={() => { }} />
       </div>
     );
