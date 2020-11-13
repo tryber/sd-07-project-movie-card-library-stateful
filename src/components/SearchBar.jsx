584c7533361a41ec8eebc5fd7927a2a08a37713c
renatomak <renato.mark.silva@gmail.com>
2020-11-13 19:41:40 -0300

    requisito 1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,30 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import MovieList from './MovieList.jsx'
+
+class SearchBar extends Component {
+  render() {
+    const { searchText,
+        onSearchTextChange,
+        bookmarkedOnly,
+        onBookmarkedChange,
+        selectedGenre,
+        onSelectedGenreChange } = this.props;
+    return (
+      <div>
+        <h2>Renato</h2>
+      </div>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string,
+  onSearchTextChange: PropTypes.func,
+  bookmarkedOnly: PropTypes.bool,
+  onBookmarkedChange: PropTypes.func,
+  selectedGenre: PropTypes.string,
+  onSelectedGenreChange: PropTypes.func,
+};
+
+export default SearchBar;
