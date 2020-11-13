e9439630c72b4190c6b6ea272df8d2730e312e7a
Samanta Below <samanta.below@gmail.com>
2020-11-13 12:56:30 -0300

    Requisitos 1 e 2

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,17 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default AddMovie;
