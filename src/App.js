8847bc19d76b760a76714bd6a9af9316f544f22f
jvitorof <j.vitoroflores@gmail.com>
2020-11-13 13:54:44 -0300

    First commit to open PR :rocket:

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React from 'react';
 import './App.css';
 
 import Header from './components/Header';
+import SearchBar from './components/SearchBar';
+import MovieList from './components/MovieList';
+
+import Movies from './data'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar />
+      <MovieList movies={Movies}/>
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,28 @@
-// implement SearchBar component here
+import React from 'react';
+
+class searchBar extends React.Component {
+constructor(props) {
+    super(props);
+    this.state = {
+        searchText: '',
+        onSearchTextChange: '',
+        bookmarkedOnly: '',
+        onBookmarkedChange: '',
+        selectedGenre: '',
+        onSelectedGenreChange: '',
+    };
+};
+
+
+render(){
+return (
+    <div>
+        <label>Text
+            <input data-item="searchText" type="text"></input>
+        </label>
+    </div>
+);
+};
+};
+     
+export default searchBar;
