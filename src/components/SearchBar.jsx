098e856f77c8368f0d8783be8e8d8d01a1196158
Lfocosi <luccafocosi@gmail.com>
2020-11-12 22:24:09 -0300

    conformese parte 2

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,21 +3,20 @@ import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
-    const {
-      searchText,
-      onSearchTextChange,
-      bookmarkedOnly,
-      onBookmarkedChange,
-      selectedGenre,
-      onSelectedGenreChange,
-    } = this.props;
+    const p = this.props;
+    const st = p.searchText;
+    const stc = p.onSearchTextChange;
+    const bk = p.bookmarkedOnly;
+    const bkc = p.onBookmarkedChange;
+    const sg = p.selectedGenre;
+    const sgc = p.onSelectedGenreChange;
     return (<form actdata-testid="search-bar-form">
       <label data-testid="text-input-label">Inclui o texto:</label>
-      <input type="text" data-testid="text-input" name="barra" value={searchText} onChange={onSearchTextChange} />
+      <input type="text" data-testid="text-input" name="barra" value={st} onChange={stc} />
       <label data-testid="checkbox-input-label">Mostrar somente favoritos</label>
-      <input type="checkbox" data-testid="checkbox-input" name="favorito" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
+      <input type="checkbox" data-testid="checkbox-input" name="fav" checked={bk} onChange={bkc} />
       <label data-testid="select-input-label">Filtrar por gênero</label>
-      <select data-testid="select-input" name="genero" value={selectedGenre} onChange={onSelectedGenreChange}>
+      <select data-testid="select-input" name="genero" value={sg} onChange={sgc}>
         <option value="" data-testid="select-option">Todos</option>
         <option value="action" data-testid="select-option">Ação</option>
         <option value="comedy" data-testid="select-option">Comédia</option>
