f056bab0def0afa545391b14e23b052528e3e6e9
murilopereira006 <muriloedu006@gmail.com>
2020-11-12 21:42:27 -0300

    CC travou

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,27 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class AddMovie extends React.Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.addMovie = this.addMovie.bind(this);
-        this.setState = {
-            subtitle: '',
-            title: '',
-            imagePath: '',
-            storyline: '',
-            rating: 0,
-            genre: 'action',
-        }
-    }
+    this.addMovie = this.addMovie.bind(this);
+    this.setState = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
 
-    render() {
-        return(
-            <form>
-
-            </form>
-        );
-    }
+  render() {
+    return (
+      <form />
+    );
+  }
 }
 
 AddMovie.propTypes = { onClick: PropTypes.func.isRequired };
