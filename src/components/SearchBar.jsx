eb1d14b8184822d289d014cfaba384b8cd3bc297
Brune Miguel <bstfreire@gmail.com>
2020-11-13 19:01:54 -0300

    Corrigindo CC

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,52 +1,55 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class SearchBar extends Component {
-  render () {
-    const { 
+  render() {
+    const {
       searchText,
       onSearchTextChange,
       bookmarkedOnly,
       onBookmarkedChange,
       selectedGenre,
-      onSelectedGenreChange
-    } = this.props
+      onSelectedGenreChange,
+    } = this.props;
 
     return (
       <div>
         <form data-testid="search-bar-form">
-          <label data-testid="text-input-label">
+          <label data-testid="text-input-label" htmlFor="text-input">
             Inclui o texto:
-            <input 
-            data-testid="text-input"
-            type="text" value={searchText}
-            onChange={onSearchTextChange}
+            <input
+              data-testid="text-input"
+              type="text" value={searchText}
+              onChange={onSearchTextChange}
             />
           </label>
 
-          <label data-testid="checkbox-input-label">
+          <label data-testid="checkbox-input-label" htmlFor="text-input">
             Mostrar somente favoritos
             <input
-            data-testid="checkbox-input"
-            type="checkbox"
-            checked={bookmarkedOnly}
-            onChange={onBookmarkedChange}
+              data-testid="checkbox-input"
+              type="checkbox"
+              checked={bookmarkedOnly}
+              onChange={onBookmarkedChange}
             />
           </label>
-          
-          <label data-testid="select-input-label">
+
+          <label data-testid="select-input-label" htmlFor="text-input">
            Filtrar por gênero
-           <select data-testid="select-input" value={selectedGenre} onChange={onSelectedGenreChange}>
+           <select
+           data-testid="select-input"
+           value={selectedGenre}
+           onChange={onSelectedGenreChange}>
               <option data-testid="select-option" value="">Todos</option>
               <option data-testid="select-option" value="action">Ação</option>
               <option data-testid="select-option" value="comedy">Comédia</option>
               <option data-testid="select-option" value="thriller">Suspense</option>
            </select>
           </label>
-        </form>  
+        </form>
       </div>
-    )
+    );
   }
 }
 
-export default SearchBar
+export default SearchBar;
 
