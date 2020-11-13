24107381cf7ca63fcb65e21ef36f93b70b7e42ce
Anderson Santana <anderslash@gmail.com>
2020-11-13 15:39:19 -0300

    Estrutura dos elementos visuais em AddMovie

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import MovieLibrary from './components/MovieLibrary';
+import AddMovie from './components/AddMovie';
 
 class App extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class App extends React.Component {
       <div className="App">
         <Header />
         <MovieLibrary />
+        <AddMovie />
       </div>
     );
   }
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,85 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  render() {
+    return (
+      <div className="movie-list">
+        <form>
+          <div>
+            <label htmlFor="title-text" data-testid="title-input-label">
+              Título
+              <input
+                name="title-text"
+                data-testid="title-input"
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="subtitle-text" data-testid="subtitle-input-label">
+              Subtítulo
+              <input
+                name="subtitle-text"
+                data-testid="subtitle-input"
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="image-url" data-testid="image-input-label">
+              Imagem
+              <input
+                name="image-url"
+                data-testid="image-input"
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="storyline-text" data-testid="storyline-input-label">
+              Sinopse
+              <textarea
+                name="storyline-text"
+                data-testid="storyline-input"
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="rating-text" data-testid="rating-input-label">
+              Avaliação
+              <input
+                name="rating-text"
+                data-testid="rating-input"
+                type="number"
+                step="0.1"
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="genre-select" data-testid="genre-input-label">
+              Gênero
+              <select
+                name="genre-select"
+                data-testid="genre-input"
+              >
+                <option value="action" data-testid="genre-option">
+                    Ação
+                </option>
+                <option value="comedy" data-testid="genre-option">
+                    Comédia
+                </option>
+                <option value="thriller" data-testid="genre-option">
+                    Suspense
+                </option>
+              </select>
+            </label>
+          </div>
+          <div>
+            <button type="submit" name="sendButton" data-testid="send-button">
+              Adicionar filme
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default AddMovie;
