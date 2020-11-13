5db1d8b18155f853d65cc74ad44228205d722c7e
Thays Costa <thayscosta3@gmail.com>
2020-11-13 18:51:18 -0300

    Search Bar

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import Rating from './Rating';
+
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
     const { title, subtitle, storyline, rating, imagePath } = movie;
     return (
-      <div className="movie-card" data-testid='movie-card'>
+      <div className="movie-card" data-testid="movie-card">
         <img alt="Movie Cover" className="movie-card-image" src={imagePath} />
         <div className="movie-card-body">
           <h4 data-testid="movie-card-title" className="movie-card-title">{title}</h4>
@@ -19,5 +20,7 @@ class MovieCard extends React.Component {
     );
   }
 }
+
+MovieCard.propTypes = { movie: PropTypes.objectOf }.isRequired;
 
 export default MovieCard;
