01d6d86ba07d8f5bd9db559b1677d3e42db3b6cc
Arthur Nunes <coletivosuburbano@gmail.com>
2020-11-13 12:23:00 -0300

    Criação das renders

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,44 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types'
+
+import AddMovieTextInputTitle from './AddMovieTextInputTitle';
+import AddMovieTextInputSubtitle from './AddMovieTextInputSubtitle';
+import AddMovieTextInputImage from './AddMovieTextInputImage';
+import AddMovieTextArea from './AddMovieTextArea';
+import AddMovieInputRating from './AddMovieInputRating';
+import AddMovieSelect from './AddMovieSelect';
+import AddMovieSendButton from './AddMovieSendButton';
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    }
+  }
+  render() {
+    return (
+      <form action="" data-testid="add-movie-form">
+        <AddMovieTextInputTitle />
+        <AddMovieTextInputSubtitle />
+        <AddMovieTextInputImage />
+        <AddMovieTextArea />
+        <AddMovieInputRating />
+        <AddMovieSelect />
+        <AddMovieSendButton />
+      </form>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+}
+
+export default AddMovie;
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,34 @@
-// implement MovieLibrary component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    const movies = this.props.movies;
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    }
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <MovieList />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+}
+
+export default MovieLibrary;
