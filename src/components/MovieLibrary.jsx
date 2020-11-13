b9768efbc1429beec71c6dbc1828406c3049492a
Vanessa Oliveira <vanessanaara@gmail.com>
2020-11-13 20:43:00 -0300

    Implementando funçoes da SearchBar e funçao de AddMovie

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import AddMovie from './AddMovie';
 import SearchBar from './SearchBar';
 import MovieList from './MovieList';
-import data from '../data';
 
 class MovieLibrary extends React.Component {
   constructor(props) {
@@ -14,43 +14,55 @@ class MovieLibrary extends React.Component {
       searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
-      movies: movies,
+      movies,
     };
 
-    // this.onSearchTextChange = this.onSearchTextChange.bind(this);
-    // this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
-    // this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
     this.addMovieHandler = this.addMovieHandler.bind(this);
   }
 
-  // onSearchTextChange() {
+  onSearchTextChange(event) {
+    const { value } = event.target;
 
-  // }
+    this.setState({ searchText: value });
+  }
 
-  // onBookmarkedChange() {
+  onBookmarkedChange() {
+    /* Referência da linha de código abaixo: stackoverflow.com/questions/
+    51480294/changing-the-state-of-a-checkbox-with-setstate/51509296 */
+    this.setState({ bookmarkedOnly: !this.state.bookmarkedOnly });
+  }
 
-  // }
-  //
-  // onSelectedGenreChange() {
-  // }
+  onSelectedGenreChange(event) {
+    const { value } = event.target;
+
+    this.setState({ selectedGenre: value });
+  }
 
   addMovieHandler(movieObject) {
-    return movieObject;
+    this.setState((previousState) => ({ movies: [...previousState.movies, movieObject] }));
   }
 
   render() {
     return (
       <div>
-        <SearchBar 
+        <SearchBar
           searchText={this.state.searchText}
           bookmarkedOnly={this.state.bookmarkedOnly}
           selectedGenre={this.state.selectedGenre}
+          onSearchTextChange={this.onSearchTextChange}
+          onBookmarkedChange={this.onBookmarkedChange}
+          onSelectedGenreChange={this.onSelectedGenreChange}
         />
-        <MovieList movies={data} />
+        <MovieList movies={this.state.movies} />
         <AddMovie onClick={this.addMovieHandler} />
       </div>
     );
   }
 }
+
+MovieLibrary.propTypes = { movies: PropTypes.array }.isRequired;
 
 export default MovieLibrary;
