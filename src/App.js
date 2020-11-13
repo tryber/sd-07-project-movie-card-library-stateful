85c109cd5c979fd2e694a9880a08f5396952b7bf
Lfocosi <luccafocosi@gmail.com>
2020-11-13 09:42:57 -0300

    search bar 100 +100

--- src/App.js
+++ src/App.js
@@ -31,7 +31,14 @@ class App extends React.Component {
     return (
       <div className="App">
         <Header />
-        <SearchBar searchText={this.state.barra} onSearchTextChange={this.onSearchTextChange} bookmarkedOnly={this.state.favorito} onBookmarkedChange={this.onBookmarkedChange} selectedGenre={this.state.genero} onSelectedGenreChange={this.onSelectedGenreChange} />
+        <SearchBar
+          searchText={this.state.barra}
+          onSearchTextChange={this.onSearchTextChange}
+          bookmarkedOnly={this.state.favorito}
+          onBookmarkedChange={this.onBookmarkedChange}
+          selectedGenre={this.state.genero}
+          onSelectedGenreChange={this.onSelectedGenreChange}
+        />
       </div>
     );
   }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,11 +5,28 @@ class SearchBar extends React.Component {
     const p = this.props;
     return (<form data-testid="search-bar-form">
       <label htmlFor="b" data-testid="text-input-label">Inclui o texto:</label>
-      <input type="text" data-testid="text-input" id="b" value={p.searchText} onChange={p.onSearchTextChange} />
+      <input
+        type="text"
+        data-testid="text-input"
+        id="b"
+        value={p.searchText}
+        onChange={p.onSearchTextChange}
+      />
       <label htmlFor="f" data-testid="checkbox-input-label">Mostrar somente favoritos</label>
-      <input type="checkbox" data-testid="checkbox-input" checked={p.bookmarkedOnly} onChange={p.onBookmarkedChange} id="f" />
+      <input
+        type="checkbox"
+        data-testid="checkbox-input"
+        checked={p.bookmarkedOnly}
+        onChange={p.onBookmarkedChange}
+        id="f"
+      />
       <label htmlFor="s" data-testid="select-input-label">Filtrar por gênero</label>
-      <select data-testid="select-input" id="s" value={p.selectedGenre} onChange={p.onSelectedGenreChange}>
+      <select
+        data-testid="select-input"
+        id="s"
+        value={p.selectedGenre}
+        onChange={p.onSelectedGenreChange}
+      >
         <option value="" data-testid="select-option">Todos</option>
         <option value="action" data-testid="select-option">Ação</option>
         <option value="comedy" data-testid="select-option">Comédia</option>
