670dc8296b5494d8dd6d54abfe74291c6a8257c4
Strongreen <strongreen@live.com>
2020-11-12 23:31:47 -0300

    correçoes CC

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -25,16 +25,24 @@ class SearchBar extends React.Component {
   render() {
     return (
       <form>
-        <label data-testid="text-input-label">Inclui o texto</label>
+        <label htmlFor="text" data-testid="text-input-label">Inclui o texto</label>
         <input
+          id="text"
           data-testid="text-input"
           value="searchText"
           type="text"
           onChange={this.onSearchTextChange}
         />
-        <input type="text" />
-        <label>Filtrar por gênero</label>
-        <input type="text" />
+        <label htmlFor="filter"> I have a car</label>
+        <input id="filter" type="checkbox" />
+
+        <label htmlFor="gender">Filtrar por gênero</label>
+        <select id="gender" type="text">
+          <option value="todos">Todos</option>
+          <option value="acao">Ação</option>
+          <option value="comedia">Comédia</option>
+          <option value="suspense">Suspense</option>
+        </select>
       </form>
     );
   }
