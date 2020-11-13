c53ccfeea4c727ddb72cc1f23fac63be4db98b11
ygorvmdf <ygorvitor.fonseca@gmail.com>
2020-11-13 12:11:30 -0300

    ajustando codeclimate

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import MovieCard from './MovieCard';
 
@@ -15,3 +16,5 @@ class MovieList extends React.Component {
 }
 
 export default MovieList;
+
+MovieList.propTypes = { movies: PropTypes.arrayOf(PropTypes.object).isRequired };
