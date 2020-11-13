ef2ff30c97649080b0ad08ae89d09c6e2e1f6076
Felipe Nascimento <flpnascto@gmail.com>
2020-11-13 01:35:29 -0300

    Definindo estado inicial do componente MovieLibrary e renderizando a SearchBar internamente.

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,10 +1,49 @@
 // implement MovieLibrary component here
 import React from 'react';
+import SearchBar from './SearchBar';
 
 class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+
+    const { movies } = this.props;
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+  }
+
+  onSearchTextChange(event) {
+    this.setState({ searchText: event.target.value });
+  }
+
+  onBookmarkedChange(event) {
+    this.setState({ bookmarkedOnly: event.target.checked });
+  }
+
+  onSelectedGenreChange(event) {
+    this.setState({ selectedGenre: event.target.value });
+  }
+
   render() {
     return (
-      <div>Olá</div>
+      <div>
+        <SearchBar
+          searchText={this.state.searchText}
+          onSearchTextChange={this.onSearchTextChange}
+          bookmarkedOnly={this.state.bookmarkedOnly}
+          onBookmarkedChange={this.onBookmarkedChange}
+          selectedGenre={this.state.selectedGenre}
+          onSelectedGenreChange={this.onSelectedGenreChange}
+        />
+      </div>
     );
   }
 }
