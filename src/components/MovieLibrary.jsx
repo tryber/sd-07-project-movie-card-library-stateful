7a3296955b6b02585761e12d552bfb4437a27ace
Nicholas Torres J. Vasconcelos <nicolastorresjatoba@gmail.com>
2020-11-13 20:55:19 -0300

    Criado componente SearchBard e suas funcionalidades, juntamento com os estados e as callbacks events de movie library

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,21 +1,43 @@
 import React from 'react';
-
-import MovieList from './MovieList';
 import SearchBar from './SearchBar';
-import AddMovie from './AddMovie';
 
-class MovieLibrary extends Component {
+class MovieLibrary extends React.Component {
   constructor(props) {
     super(props);
+
+    this.eventHandlerChange = this.eventHandlerChange.bind(this);
+
+    this.state = {
+      textInput: '',
+      markedBook: false,
+      genreSelected: '',
+    };
+  }
+
+  eventHandlerChange({ target: { name, value, checked } }) {
+    let theValue = value;
+    if (name === 'markedBook') {
+      theValue = checked;
+    }
+    this.setState({ [name]: theValue });
   }
 
   render() {
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar />
+        <SearchBar
+          searchText={this.state.textInput}
+          onSearchTextChange={this.eventHandlerChange}
+          bookmarkedOnly={this.state.markedBook}
+          onBookmarkedChange={this.eventHandlerChange}
+          selectedGenre={this.state.genreSelected}
+          onSelectedGenreChange={this.eventHandlerChange}
+        />
+        {/*
         <MovieList movies={this.props.movies} />
         <AddMovie />
+        */}
       </div>
     );
   }
