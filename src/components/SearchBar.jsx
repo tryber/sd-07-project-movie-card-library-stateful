69e3b059a3937c721796ff0e583e204be7b40cae
jvitorof <j.vitoroflores@gmail.com>
2020-11-13 16:15:25 -0300

    Req-SearchBar :rocket::metal:

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,31 +1,50 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-class searchBar extends React.Component {
-constructor(props) {
-    super(props);
-    this.state = {
-        searchText: '',
-        onSearchTextChange: '',
-        bookmarkedOnly: '',
-        onBookmarkedChange: '',
-        selectedGenre: '',
-        onSelectedGenreChange: '',
+class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchText: '',
+            onSearchTextChange: '',
+            bookmarkedOnly: '',
+            onBookmarkedChange: '',
+            selectedGenre: '',
+            onSelectedGenreChange: '',
+        };
+    };
+
+    render(){
+        return (
+            <div>
+                <form data-testid="search-bar-form" >
+                    <label data-testid="text-input-label">Inclui o texto:
+                        <input data-testid="text-input" value={this.props.searchText} onChange={this.props.onSearchTextChange} type="text"></input>
+                    </label>
+                    <label data-testid="checkbox-input-label">Mostrar somente favoritos:
+                        <input data-testid="checkbox-input" checked={this.props.bookmarkedOnly} onChange={this.props.onBookmarkedChange} type="checkbox"></input>
+                    </label>
+                    <label data-testid="select-input-label">Filtrar por gênero
+                        <select data-testid="select-input" value={this.props.selectedGenre} onChange={this.props.onSelectedGenreChange} type="checkbox">
+                            <option data-testid="select-option" value="">Todos</option>
+                            <option data-testid="select-option" value="action">Ação</option>
+                            <option data-testid="select-option" value="comedy">Comédia</option>
+                            <option data-testid="select-option" value="thriller">Suspense</option>
+                        </select>
+                    </label>
+                </form>
+            </div>
+        );
     };
 };
 
-Filter = (event) => {
-    console.log(event.target.value);
-}
+SearchBar.propTypes = {
+    searchText: PropTypes.string.isRequired,
+    onSearchTextChange: PropTypes.func.isRequired,
+    bookmarkedOnly: PropTypes.bool.isRequired,
+    onBookmarkedChange: PropTypes.func.isRequired,
+    selectedGenre: PropTypes.string.isRequired,
+    onSelectedGenreChange: PropTypes.func.isRequired
+};
 
-render(){
-return (
-    <div>
-        <label>Text
-            <input data-item="searchText" type="text"></input>
-        </label>
-    </div>
-);
-};
-};
-     
-export default searchBar;
+export default SearchBar;
