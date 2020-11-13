6f26cb2642dddd376a66422c68ef3e42d94fa221
TIAGO <tiagoesdrasvieira@gmail.com>
2020-11-13 17:28:25 -0300

    Renderizando um form em SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 class SearchBar extends React.Component {
   render() {
-    return (<div />);
+    return (
+      <form data-testid="search-bar-form" />
+    );
   }
 }
 
