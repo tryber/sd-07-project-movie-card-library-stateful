ba2610f09a79f83ad6d62b3c0b40c3681d320194
Samanta Below <samanta.below@gmail.com>
2020-11-13 19:17:13 -0300

    AddMovie - inputs do forms completos

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -28,6 +28,7 @@ class AddMovie extends Component {
         <form data-testid="add-movie-form">
           <label htmlFor="title-input" data-testid="title-input-label">Título</label>
           <input
+            type="text"
             data-testid="title-input"
             onChange={this.handleChange}
             value={this.state.title}
@@ -35,6 +36,7 @@ class AddMovie extends Component {
           </input>
           <label htmlFor="subtitle-input" data-testid="subtitle-input-label">Subtítulo</label>
           <input
+            type="text"
             data-testid="subtitle-input"
             onChange={this.handleChange}
             value={this.state.subtitle}
@@ -42,18 +44,51 @@ class AddMovie extends Component {
           </input>
           <label htmlFor="image-input" data-testid="image-input-label">Imagem</label>
           <input
+            type="text"
             data-testid="image-input"
             onChange={this.handleChange}
             value={this.state.imagePath}
           >
           </input>
           <label htmlFor="storyline-input" data-testid="storyline-input-label">Sinopse</label>
-          <input
+          <textarea
             data-testid="storyline-input"
             onChange={this.handleChange}
             value={this.state.storyline}
+          />
+          <label htmlFor="rating-input" data-testid="rating-input-label">Avaliação</label>
+          <input
+            type="number"
+            data-testid="rating-input"
+            onChange={this.handleChange}
+            value={this.state.rating}
           >
           </input>
+          <label htmlFor="genre-input" data-testid="genre-input-label">Gênero</label>
+          <select
+            data-testid="genre-input"
+            onChange={this.handleChange}
+            value={this.state.genre}
+          >
+            <option
+              data-testid="genre-option"
+              value="action"
+            >
+            Ação
+            </option>
+            <option
+              data-testid="genre-option"
+              value="comedy"
+            >
+            Comédia
+            </option>
+            <option
+              data-testid="genre-option"
+              value="thriller"
+            >
+            Suspense
+            </option>
+          </select>
         </form>
       </div>
     );
