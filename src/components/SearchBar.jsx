91edbd9bbf6f7c3081444b41a0a32ce03b12b024
Strongreen <strongreen@live.com>
2020-11-12 22:49:36 -0300

    SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,43 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+    }
+  }
+
+  onSearchTextChange = () => {
+    return 'teste'
+  }
+
+   onBookmarkedChange = () => {
+    return 'teste'
+  }
+
+   onSelectedGenreChange = () => {
+    return 'teste'
+  }
+
   render() {
-    return (<p>Barra de pesquisa</p>);
+    return (
+      <form>
+        <label data-testid="text-input-label">
+          Inclui o texto
+        </label>
+        <input data-testid="text-input" 
+          value="searchText" 
+          type="text" 
+          onChange={this.onSearchTextChange}
+          />
+        <input type="text" />
+        <label>Filtrar por gênero</label>
+        <input type="text" / >
+      </form>
+    );
   }
 }
 
