4049c68f795a5d59b14c9579d10aacbd0e16ba18
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2020-11-13 01:04:25 -0300

    We are not alone...

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,19 +6,30 @@ import SearchBar from './SearchBar';
 class MovieList extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      titleFind: '',
+      favorit: false,
+      genere: ''
+    };
+    this.handleChange = this.handleChange.bind();
+  }
+
+  handleChange({target}) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+      // this.setState({ [name]: value });
   }
   render() {
     const { movies } = this.props;
     return (
       <div>
         <SearchBar
-          searchText=""
-          onSearchTextChange=""
-          bookmarkedOnly=""
-          onBookmarkedChange=""
-          selectedGenre=""
-          onSelectedGenreChange=""
+          searchText={this.state.titleFind}
+          onSearchTextChange={this.handleChange}
+          bookmarkedOnly={this.state.favorits}
+          onBookmarkedChange={this.handleChange}
+          selectedGenre={this.state.genere}
+          onSelectedGenreChange={this.handleChange}
         />
         <div data-testid="movie-list" className="movie-list">
           {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
