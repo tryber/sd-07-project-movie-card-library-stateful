2c5fb95a231015b1a8abdd38fc3831d02ab1a5c9
Bruna Fonsêca <1998bfonseca@gmail.com>
2020-11-13 17:21:52 -0300

    some changes

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,71 @@
-// implement MovieLibrary component here
+import React from 'react';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const { movies } = this.props;
+
+    this.textChange = this.textChange.bind(this);
+    this.markedBook = this.markedBook.bind(this);
+    this.genderSelectedChange = this.genderSelectedChange.bind(this);
+    this.moviesFiltered = this.moviesFiltered.bind(this);
+    
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    }
+  }
+  
+  textChange({ target }) {
+    this.setState({ searchText: target.value });
+  }
+
+  markedBook({ target }) {
+    this.setState({ bookmarkedOnly: target.checked });
+  }
+
+  genderSelectedChange({ target }) {
+    this.setState({ selectedGenre: target.value });
+  }
+
+  moviesFiltered() {
+    if (this.state.bookmarkedOnly === true) {
+      return this.state.movies
+      .filter((movie) => movie.bookmarkedOnly === true);
+    } else if (this.state.selectedGenre !== '') {
+      return this.state.movie
+      .filter((movie) => movie.genre === this.state.selectedGenre);
+    }
+    return this.state.movies
+      .filter((movie) => 
+        movie.title.includes(this.state.searchText) ||
+        movie.subtitle.includes(this.state.searchText) ||
+        movie.storyline.includes(this.state.searchText));
+  }
+
+  render() {
+
+    return (
+    <div>
+      <SearchBar 
+        searchText={this.state.searchText}
+        bookmarkedOnly={this.state.bookmarkedOnly}
+        selectedGenre={this.state.selectedGenre}
+        onSearchTextChange={this.textChange}
+        onBookmarkedChange={this.markedBook}
+        selectedGenreGhange={this.genderSelectedChange}
+      />
+      <MovieList movies={this.moviesFiltered()} />
+      <AddMovie />
+    </div>)
+  }
+}
+
+export default MovieLibrary;
