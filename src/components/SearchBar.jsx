558e270220b17dc80cd93128e995fb880c7e8562
Samanta Below <samanta.below@gmail.com>
2020-11-13 13:31:40 -0300

    SearchBar completo

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types'; 
+import PropTypes from 'prop-types';
 
 class SearchBar extends Component {
   render() {
-    const { searchText,
+    const {
+      searchText,
       onSearchTextChange,
       bookmarkedOnly,
       onBookmarkedChange,
       selectedGenre,
-      onSelectedGenreChange, } = this.props;
+      onSelectedGenreChange,
+    } = this.props;
     return (
       <div>
         <form data-testid="search-bar-form">
-
+          <label data-testid="text-input-label" htmlFor="text-input">Inclui o texto:</label>
+          <input
+            type="text"
+            value={searchText}
+            onChange={onSearchTextChange}
+            data-testid="text-input"
+          />
+          <label data-testid="checkbox-input-label" htmlFor="checkbox-input">Mostrar somente favoritos</label>
+          <input
+            type="checkbox"
+            checked={bookmarkedOnly}
+            onChange={onBookmarkedChange}
+            data-testid="checkbox-input"
+          />
+          <label data-testid="select-input-label" htmlFor="select-input">Filtrar por gênero</label>
+          <select
+            value={selectedGenre}
+            onChange={onSelectedGenreChange}
+            data-testid="select-input"
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
         </form>
-        {searchText}
-        {onSearchTextChange}
-        {bookmarkedOnly}
-        {onBookmarkedChange}
-        {selectedGenre}
-        {onSelectedGenreChange}
       </div>
     );
   }
