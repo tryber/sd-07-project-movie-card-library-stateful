3a192744b916baf6419664c19c174d49f0e2fa7c
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2020-11-13 15:42:19 -0300

    Finalização componente SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,6 @@
 // implement SearchBar component here
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
@@ -27,6 +27,7 @@ class SearchBar extends React.Component {
             />
           </label>
           <label htmlFor="checkbox-input" data-testid="checkbox-input-label">
+            Mostrar somente favoritos
             <input
               type="checkbox"
               onChange={onBookmarkedChange}
@@ -35,10 +36,40 @@ class SearchBar extends React.Component {
               data-testid="checkbox-input"
             />
           </label>
+          <label htmlFor="select-input" data-testid="select-input-label">
+            Filtrar por gênero
+            <select
+              id="select-input"
+              value={selectedGenre}
+              onChange={onSelectedGenreChange}
+              data-testid="select-input"
+            >
+              <option value="" data-testid="select-option">
+                Todos
+              </option>
+              <option value="action" data-testid="select-option">
+                Ação
+              </option>
+              <option value="comedy" data-testid="select-option">
+                Comédia
+              </option>
+              <option value="thriller" data-testid="select-option">
+                Suspense
+              </option>
+            </select>
+          </label>
         </form>
       </div>
     );
   }
 }
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+};
 
 export default SearchBar;
