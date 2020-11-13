64fff50886a1fab39afe7b944bfe1e6500a937ac
Strongreen <strongreen@live.com>
2020-11-13 17:53:02 -0300

    AddMovie title

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,49 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      subtitle: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick(event) {
+    this.setState({ title: event.target.value });
+    this.setState({ subtitle: event.target.value });
+  }
+  render() {
+    return (
+      <form >
+        <label htmlFor="title" data-testid="title-input-label">
+          Título
+        </label>
+        <input
+          id="title"
+          data-testid="title-input"
+          value={this.title}
+          type="text"
+          onChange={this.onClick}
+        />
+        <label htmlFor="subtitle" data-testid="subtitle-input-label">
+          Título
+        </label>
+        <input
+          id="subtitle"
+          data-testid="subtitle-input"
+          value={this.subtitle}
+          type="text"
+          onChange={this.onClick}
+        />        
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
