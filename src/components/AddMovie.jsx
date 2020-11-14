3438d380c1390d2c1ede0676709ee34f1f575b9a
brendalima <brenda.c.lima@poli.ufrj.br>
2020-11-14 20:42:02 -0300

    adding data-testid atribute to AddMovie.jsx

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,42 @@
 // implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  // constructor() {
+  //   super();
+  //   this.updateStatus = this.updateStatus.bind(this);
+  //   this.state = {
+  //     subtitle: '',
+  //     title: '',
+  //     imagePath: '',
+  //     storyline: '',
+  //     rating: 0,
+  //     genre: 'action',
+  //   };
+  // }
+
+  // updateStatus(event) {
+  //   this.setState({
+  //     title: event.target.value,
+  //   })
+  // }
+
+  render () {
+    return (
+      <form data-testid="add-movie-form">
+        {/* <label htmlFor="titulo" data-testid="title-input-label">
+          Título
+        </label>
+        <input
+        type="text"
+        id="titulo"
+        value={this.state.title}
+        data-testid="title-input"
+        onChange={updateStatus}
+        /> */}
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
