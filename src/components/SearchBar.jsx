c76f1be55211b50ff7b6a3a21b060b4a5100492c
André Horman de Medeiros Correia <tavernamoderna8000@gmail.com>
2020-11-14 12:12:59 -0300

    Alterando aspas simples - 14/11/2020

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,6 @@
 // implement SearchBar component here
 import React from "react";
+import PropTypes from 'prop-types'
 
 class SearchBar extends React.Component {
   render() {
@@ -67,6 +68,24 @@ class SearchBar extends React.Component {
       </form>
     );
   }
+}
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired
+}
+
+SearchBar.defaultProps = {
+  searchText: "",
+  onSearchTextChange: () => {},
+  bookmarkedOnly: false,
+  onBookmarkedChange: () => {},
+  selectedGenre: "",
+  onSelectedGenreChange: () => {}
 }
 
 export default SearchBar;
