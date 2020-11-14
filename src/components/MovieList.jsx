cda58587509e40ae3b1818d5ad8c98403185791c
Carol Bezerra <carol.arbe@gmail.com>
2020-11-14 20:19:23 -0300

    SearchBar

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-import MovieCard from './MovieCard';
+// import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    const { movies } = this.props;
+    // const { movies } = this.props;
 
     return (
       <div data-testid="movie-list" className="movie-list">
-        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+        {/* {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)} */}
       </div>
     );
   }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 class Rating extends React.Component {
   render() {
-    const { rating } = this.props;
+    // const { rating } = this.props;
     return (
       <div className="movie-card-rating" data-testid="rating">
-        <span className="rating">{rating}</span>
+        {/* <span className="rating">{rating}</span> */}
       </div>
     );
   }
