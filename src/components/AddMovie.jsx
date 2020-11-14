1f2705c03d5bb9c96294e049b167792d59960f73
paula-jfe <paula.jfe@gmail.com>
2020-11-13 22:55:08 -0300

    CC ok

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,117 @@
 // implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+    this.addNewMovie = this.addNewMovie.bind(this);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  handleChange(event) {
+    const name = event.target.name;
+    const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
+    this.setState({ [name]: value });
+  }
+
+  addNewMovie() {
+    const newMovie = this.state;
+    const onClick = this.props.onClick;
+    onClick(newMovie);
+
+    this.setState({
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <form data-testid="add-movie-form">
+          <label htmlFor="input" data-testid="title-input-label">
+            Título
+            <input
+              data-testid="title-input"
+              type="text" name="title"
+              value={this.state.title}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label htmlFor="input" data-testid="subtitle-input-label">
+            Subtítulo
+            <input
+              data-testid="subtitle-input"
+              type="text"
+              name="subtitle"
+              value={this.state.subtitle}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label htmlFor="input" data-testid="image-input-label">
+            Imagem
+            <input
+              data-testid="image-input"
+              type="text"
+              name="imagePath"
+              value={this.state.imagePath}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label htmlFor="input" data-testid="storyline-input-label">
+            Sinopse
+            <textarea
+              data-testid="storyline-input"
+              name="storyline"
+              value={this.state.storyline}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label htmlFor="input" data-testid="rating-input-label">
+            Avaliação
+            <input
+              data-testid="rating-input"
+              type="number"
+              name="rating"
+              value={this.state.rating}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label htmlFor="select" data-testid="genre-input-label">
+            Gênero
+            <select
+              data-testid="genre-input"
+              name="genre"
+              value={this.state.genre}
+              onChange={this.handleChange}
+            >
+              <option data-testid="genre-option" value="action">Ação</option>
+              <option data-testid="genre-option" value="comedy">Comédia</option>
+              <option data-testid="genre-option" value="thriller">Suspense</option>
+            </select>
+          </label>
+          <button data-testid="send-button" onClick={() => this.addNewMovie()}>
+            Adicionar filme
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+AddMovie.propTypes = { onClick: PropTypes.func.isRequired };
+
+export default AddMovie;
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,102 @@
 // implement MovieLibrary component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import AddMovie from './AddMovie';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props); /* Chama o construtor do React.Component */
+    this.handleChange = this.handleChange.bind(this);
+    this.addNewMovie = this.addNewMovie.bind(this);
+    this.filterMovies = this.filterMovies.bind(this);
+    this.filteredGenreFuncion = this.filteredGenreFuncion.bind(this);
+    this.filteredBookMarkedFunction = this.filteredBookMarkedFunction.bind(this);
+    this.filteredTextFunction = this.filteredTextFunction.bind(this);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
+    };
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
+  filteredGenreFuncion(chosenGenre, moviesArray) {
+    if (chosenGenre !== '') {
+      return moviesArray.filter((movie) => movie.genre === chosenGenre);
+    }
+    return moviesArray;
+  }
+
+  filteredBookMarkedFunction(filteredGenre, isBookMarked) {
+    if (isBookMarked !== false) {
+      return filteredGenre.filter((movie) => movie.bookmarked === true);
+    }
+    return filteredGenre;
+  }
+
+  filteredTextFunction(filteredBookMarked, chosenText) {
+    if (chosenText !== '') {
+      return filteredBookMarked.filter((movie) =>
+      movie.title.toLowerCase().includes(chosenText.toLowerCase()) === true ||
+      movie.subtitle.toLowerCase().includes(chosenText.toLowerCase()) === true ||
+      movie.storyline.toLowerCase().includes(chosenText.toLowerCase()) === true);
+    }
+    return filteredBookMarked;
+  }
+
+  filterMovies() {
+    const moviesArray = this.state.movies;
+    const chosenGenre = this.state.selectedGenre;
+    const isBookMarked = this.state.bookmarkedOnly;
+    const chosenText = this.state.searchText;
+    const filteredGenre = this.filteredGenreFuncion(chosenGenre, moviesArray);
+    const filteredBookMarked = this.filteredBookMarkedFunction(filteredGenre, isBookMarked);
+    const filteredText = this.filteredTextFunction(filteredBookMarked, chosenText);
+
+    console.log(filteredText);
+    return filteredText;
+    /* Colocar novo estado */
+  }
+
+/*   addNewMovie(newObject) {
+    const newMovie = {
+      title: newObject.title,
+      subtitle: newObject.subtitle,
+      storyline: newObject.imagePath,
+      rating: newObject.storyline,
+      imagePath: newObject.rating,
+      genre: newObject.genre,
+    };
+
+    const moviesOldState = this.state.movies;
+    this.setState((oldestArray) => ({ moviesOldState: [...oldestArray.movies, newObject] }));
+  } */
+
+  render() {
+    return (
+      <div>
+        <SearchBar
+          searchText={this.state.searchText}
+          onSearchTextChange={this.handleChange}
+          bookmarkedOnly={this.state.bookmarkedOnly}
+          onBookmarkedChange={this.handleChange}
+          selectedGenre={this.state.selectedGenre}
+          onSelectedGenreChange={this.handleChange}
+        />
+        <AddMovie onClick={this.addNewMovie} />
+        <MovieList movies={this.filterMovies()} />
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = { movies: PropTypes.arrayOf(PropTypes.object).isRequired };
+
+export default MovieLibrary;
