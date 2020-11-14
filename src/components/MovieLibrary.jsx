ede137b9d38b4adb8b44d83d9755e7704518abf4
Strongreen <strongreen@live.com>
2020-11-14 17:09:00 -0300

    movieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
 
 class MovieLibrary extends React.Component {
+
+
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: [],
+    };
+  }
+
   render() {
+    const { movies } = this.props;
     return (
       <div>
-        Barra de pesquisa
-      </div>);
+        <SearchBar />
+        <MovieList movies={movies} />
+      </div>
+    );
   }
 }
+
+MovieLibrary.propTypes = { movies: PropTypes.arrayOf(PropTypes.objectOf).isRequired };
 
 export default MovieLibrary;
