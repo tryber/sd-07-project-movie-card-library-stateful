06e4e46e7a38d83c1facdb32217484da93acd8e9
renatomak <renato.mark.silva@gmail.com>
2020-11-14 11:00:22 -0300

    Renderize um input do tipo texto dentro do formulário em SearchBar-4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,31 +1,41 @@
 import React, { Component } from 'react';
-/* import PropTypes from 'prop-types';
-import MovieList from './MovieList.jsx' */
-import Form from './Form';
+import PropTypes from 'prop-types';
+/* import MovieList from './MovieList.jsx' */
+
 
 class SearchBar extends Component {
   render() {
-    /* const { searchText,
-      onSearchTextChange,
+    const {
+      searchText,
+      /* onSearchTextChange,
       bookmarkedOnly,
       onBookmarkedChange,
       selectedGenre,
-      onSelectedGenreChange } = this.props; */
+      onSelectedGenreChange */ } = this.props;
     return (
       <div>
-        <Form searchText="My Text" />
+        <form data-testid="search-bar-form">
+          <label htmlFor="text-input" data-testid="text-input-label">
+              Inclui o texto:
+            <input
+              type="text"
+              value={searchText}
+              onChange="onSearchTextChange"
+              data-testid="text-input"
+              id="text-input"
+            />
+          </label>
+        </form>
       </div>
     );
   }
 }
 
-/* SearchBar.propTypes = {
-  searchText: PropTypes.string,
-  onSearchTextChange: PropTypes.func,
+SearchBar.propTypes = { searchText: PropTypes.string.isRequired };
+
+/* onSearchTextChange: PropTypes.func,
   bookmarkedOnly: PropTypes.bool,
   onBookmarkedChange: PropTypes.func,
   selectedGenre: PropTypes.string,
-  onSelectedGenreChange: PropTypes.func,
-}; */
-
+  onSelectedGenreChange: PropTypes.func, */
 export default SearchBar;
