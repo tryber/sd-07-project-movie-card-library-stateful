62d3f94cf4d3334b62b350e22b4f1e048f93b78d
josielvs <josielvs@gmail.com>
2020-11-14 15:37:36 -0300

    Send requirement Header and SearchBar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
 import React from 'react';
+import './libs/bulma.min.css'
 import './App.css';
-
+// import data from './data';
 import Header from './components/Header';
+import SearchBar from './components/SearchBar';
+import MovieLibrary from './components/MovieLibrary';
 
-function App() {
-  return (
-    <div className="App">
-      <Header class="header" />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      searchText: '',
+      onSearchTextChange: (event) => {
+        this.setState({
+            searchText: event.target.value
+        })
+      },
+      bookmarkedOnly: true,
+      onBookmarkedChange: (event) => {
+        this.setState({
+          bookmarkedOnly: event.target.value
+        })
+        console.log(event.target)
+      },
+      selectedGenre: '',
+      onSelectedGenreChange: (event) => {
+        this.setState({
+          selectedGenre: event.target.value
+        })
+        console.log(event.target.value)
+      },
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <SearchBar
+          searchText={this.state.searchText} 
+          onSearchTextChange={this.state.onSearchTextChange}
+          bookmarkedOnly={this.state.bookmarkedOnly}
+          onBookmarkedChange={this.state.onBookmarkedChange}
+          />
+          <MovieLibrary />
+      </div>
+    );
+  }
 }
 
 export default App;
