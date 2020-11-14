df580643101e78ad8e3a920d763b430aa241f744
Rogerio-feitor <roger.feitor@gmail.com>
2020-11-14 09:33:23 -0300

    Depois de todos os testes terem sido aprovados, vamos ver como fica.

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -13,6 +13,6 @@ class Rating extends React.Component {
 }
 
 Rating.defaultProps = { rating: 0 };
-Rating.propTypes = { rating: PropTypes.number.isRequired};
+Rating.propTypes = { rating: PropTypes.number.isRequired };
 
 export default Rating;
