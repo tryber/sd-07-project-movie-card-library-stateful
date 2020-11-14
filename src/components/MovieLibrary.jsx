2f662ceed32484775aed443044301f89a20b9723
André Horman de Medeiros Correia <tavernamoderna8000@gmail.com>
2020-11-14 12:33:10 -0300

    Alterando aspas simples - 14/11/2020

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,5 +1,6 @@
 // implement MovieLibrary component here
 import React from 'react';
+import PropTypes from 'prop-types';
 import SearchBar from './SearchBar';
 import MovieList from './MovieList';
 import AddMovie from './AddMovie';
@@ -75,5 +76,9 @@ class MovieLibrary extends React.Component {
     );
   }
 }
+
+MovieLibrary.propTypes = { movies: PropTypes.arrayOf(PropTypes.object).isRequired };
+
+MovieLibrary.defaultProps = { movies: [] };
 
 export default MovieLibrary;
