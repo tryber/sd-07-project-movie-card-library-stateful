d6cd1a5b55b7e779a4da90499daf2630c316e2f1
Brune Miguel <bstfreire@gmail.com>
2020-11-14 18:59:36 -0300

    Corrigindo requisito AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -14,6 +14,7 @@ class AddMovie extends Component {
       genre: 'action',
     };
     this.handleChange = this.handleChange.bind(this);
+    this.resetAndSendState = this.resetAndSendState.bind(this);
   }
 
   handleChange({ target }) {
@@ -23,13 +24,31 @@ class AddMovie extends Component {
     this.setState({ [name]: value });
   }
 
+  resetAndSendState() {
+    this.props.onClick(this.state);
+
+    this.setState({
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    });
+  }
+
   render() {
     return (
       <div>
-        <form>
-          <label data-testid="title-input-label" htmlFor="text-input">
+        <form
+          data-testid="add-movie-form"
+          action=""
+          method="get"
+        >
+          <label data-testid="title-input-label" htmlFor="title-input">
             Título
             <input
+              name="title"
               data-testid="title-input"
               type="text"
               value={this.state.title}
@@ -37,9 +56,10 @@ class AddMovie extends Component {
             />
           </label>
 
-          <label data-testid="subtitle-input-label" htmlFor="text-input">
+          <label data-testid="subtitle-input-label" htmlFor="subtitle-input">
             Subtítulo
             <input
+              name="subtitle"
               data-testid="subtitle-input"
               type="text"
               value={this.state.subtitle}
@@ -47,52 +67,56 @@ class AddMovie extends Component {
             />
           </label>
 
-          <label data-testid="image-input-label" htmlFor="text-input">
+          <label data-testid="image-input-label"  htmlFor="image-input">
             Imagem
             <input
               data-testid="image-input"
+              name="imagePath"
               type="text"
               value={this.state.imagePath}
               onChange={this.handleChange}
             />
           </label>
 
-          <label data-testid="storyline-input-label" htmlFor="text-input">
+          <label data-testid="storyline-input-label" htmlFor="storyline">
             Sinopse
             <textarea
               data-testid="storyline-input"
-              required
+              name="storyline"
               value={this.state.storyline}
               onChange={this.handleChange}
             />
           </label>
 
-          <label data-testid="rating-input-label" htmlFor="text-input">
+          <label data-testid="rating-input-label" htmlFor="rating-input">
             Avaliação
             <input
               data-testid="rating-input"
+              name="rating"
               type="number"
               value={this.state.rating}
               onChange={this.handleChange}
             />
           </label>
 
-          <label data-testid="genre-input-label" htmlFor="text-input">
-           Gênero
+          <label data-testid="genre-input-label" htmlFor="genre-input">
+            Gênero
            <select
-             data-testid="genre-input"
-             value={this.state.genre}
-             onChange={this.handleChange}
-           >
-             <option data-testid="genre-option" value="action">Ação</option>
-             <option data-testid="genre-option" value="comedy">Comédia</option>
-             <option data-testid="genre-option" value="thriller">Suspense</option>
-           </select>
+              data-testid="genre-input"
+              name="genre"
+              value={this.state.genre}
+              onChange={this.handleChange}
+            >
+              <option data-testid="genre-option" value="action">Ação</option>
+              <option data-testid="genre-option" value="comedy">Comédia</option>
+              <option data-testid="genre-option" value="thriller">Suspense</option>
+            </select>
           </label>
 
           <button
             data-testid="send-button"
             type="button"
+            onClick={this.resetAndSendState}
           >Adicionar filme</button>
 
         </form>
@@ -101,14 +125,6 @@ class AddMovie extends Component {
   }
 }
 
-AddMovie.propTypes = {
-  subtitle: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  imagePath: PropTypes.string.isRequired,
-  storyline: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-  genre: PropTypes.func.isRequired,
-};
+AddMovie.propTypes = { onClick: PropTypes.func.isRequired };
 
 export default AddMovie;
-
