ab76012fc8694a66e3de24a4a7ccdc7c9156ab1d
Rogerio-feitor <roger.feitor@gmail.com>
2020-11-13 21:39:48 -0300

    Depois de apanhar muito do codeclimate e ter que excluir os arquivos de teste da minha pasta, acho de adiantei alguns requisitos.

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
-    const { rating } = this.props;
+    const { rating } = this.props.rating;
     return (
       <div className="movie-card-rating" data-testid="rating">
         <span className="rating">{rating}</span>
@@ -10,5 +11,7 @@ class Rating extends React.Component {
     );
   }
 }
+
+Rating.propTypes = { rating: PropTypes.number.isRequired };
 
 export default Rating;
