4f09962b0f868828fdd0d13ee30e18e8ad415489
renatomak <renato.mark.silva@gmail.com>
2020-11-14 09:16:39 -0300

    Renderize um formulário dentro de SearchBar

---
+++ src/components/Form.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+  render() {
+    return (
+      <div>
+        <form data-testid="search-bar-form" />
+      </div>
+    );
+  }
+}
+export default Form;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
+/* import PropTypes from 'prop-types';
+import MovieList from './MovieList.jsx' */
+import Form from './Form.jsx';
 
 class SearchBar extends Component {
   render() {
+    /* const { searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange } = this.props; */
     return (
       <div>
-        <h2>teste</h2>
+        <Form />
       </div>
     );
   }
 }
+
+/* SearchBar.propTypes = {
+  searchText: PropTypes.string,
+  onSearchTextChange: PropTypes.func,
+  bookmarkedOnly: PropTypes.bool,
+  onBookmarkedChange: PropTypes.func,
+  selectedGenre: PropTypes.string,
+  onSelectedGenreChange: PropTypes.func,
+}; */
 
 export default SearchBar;
