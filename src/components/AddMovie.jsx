0b9e388f79c5fcaaa33cb78ab1ea0b340ebf9b19
CleberFontinele <cleberfontinele2387@gmail.com>
2020-11-14 18:16:52 -0300

    codeclimate issues

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -25,7 +25,10 @@ class AddMovie extends React.Component {
   handleChange({ target }) {
     const { name } = target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
-    this.setState({ [name]: value });
+    this.setState((beforeState) => ({
+      ...beforeState,
+      [name]: value,
+    }));
   }
 
   render() {
