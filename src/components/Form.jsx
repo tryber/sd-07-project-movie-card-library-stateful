5a7a11006bf5a798234f7c7d61230abb3f0925b5
renatomak <renato.mark.silva@gmail.com>
2020-11-14 10:01:37 -0300

    Renderize um input do tipo texto dentro do formulário em SearchBar

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,7 +4,10 @@ class Form extends Component {
   render() {
     return (
       <div>
-        <form data-testid="search-bar-form" />
+        <form data-testid="search-bar-form">
+          <label for="text-input" data-testid="text-input-label">Inclui o texto:</label>
+          <input type="text" value="searchText" onChange="onSearchTextChange" data-testid="text-input" />
+        </form>
       </div>
     );
   }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 /* import PropTypes from 'prop-types';
 import MovieList from './MovieList.jsx' */
-import Form from './Form.jsx';
+import Form from './Form';
 
 class SearchBar extends Component {
   render() {
