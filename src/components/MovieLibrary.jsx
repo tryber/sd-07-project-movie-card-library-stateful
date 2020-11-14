4a0ccb38b4e8fbc24bcb26e24695b832d3052199
spawnred <bruno.spawnhell@gmail.com>
2020-11-14 16:08:18 -0300

    estou tentando fazer mais commits

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
 
 class MovieLibrary extends React.Component {
   render() {
     const { movies } = this.props;
     return (
       <div>
+        <SearchBar />
         <MovieList movies={movies} />
+        <AddMovie />
       </div>
     );
   }
