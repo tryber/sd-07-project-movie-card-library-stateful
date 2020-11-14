8d716ae49c484110431ce0266f755e3e4898eb42
Brune Miguel <bstfreire@gmail.com>
2020-11-14 19:05:23 -0300

    Corrigindo CC

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -67,7 +67,7 @@ class AddMovie extends Component {
             />
           </label>
 
-          <label data-testid="image-input-label"  htmlFor="image-input">
+          <label data-testid="image-input-label" htmlFor="image-input">
             Imagem
             <input
               data-testid="image-input"
@@ -102,15 +102,15 @@ class AddMovie extends Component {
           <label data-testid="genre-input-label" htmlFor="genre-input">
             Gênero
            <select
-              data-testid="genre-input"
-              name="genre"
-              value={this.state.genre}
-              onChange={this.handleChange}
-            >
-              <option data-testid="genre-option" value="action">Ação</option>
-              <option data-testid="genre-option" value="comedy">Comédia</option>
-              <option data-testid="genre-option" value="thriller">Suspense</option>
-            </select>
+            data-testid="genre-input"
+            name="genre"
+            value={this.state.genre}
+            onChange={this.handleChange}
+           >
+            <option data-testid="genre-option" value="action">Ação</option>
+            <option data-testid="genre-option" value="comedy">Comédia</option>
+            <option data-testid="genre-option" value="thriller">Suspense</option>
+           </select>
           </label>
 
           <button
