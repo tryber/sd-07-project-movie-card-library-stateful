ff60be66f76f00f19ecc021d54ac4b95fe1ac690
Strongreen <strongreen@live.com>
2020-11-14 18:29:31 -0300

    correçao add movie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -9,7 +9,7 @@ class AddMovie extends React.Component {
       imagePath: '',
       storyline: '',
       rating: 0,
-      genre: 'select',
+      genre: 'action',
     };
     this.onClick = this.onClick.bind(this);
   }
