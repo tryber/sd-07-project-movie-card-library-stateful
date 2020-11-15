9c350690cf6b225d22f4ef75a58be7879a8178ae
sttefanieMaciel <macielsttefanie@gmail.com>
2020-11-15 15:35:14 -0300

    Criado o componente searchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,12 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>SearchBar</h1>
+      </header>
+    );
+  }
+}
+export default SearchBar;
