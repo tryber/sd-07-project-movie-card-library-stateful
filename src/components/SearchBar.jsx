b503833c3e2b440aeefe0075208461def171e949
Quintiliano Dalete <quintilianodalete@gmail.com>
2020-11-15 11:22:29 -0300

    Ajusting code climate

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -35,39 +35,40 @@ class SearchBar extends Component {
           <input
             id="favorits-input"
             data-testid="checkbox-input"
-            onChange={ onBookmarkedChange }
+            onChange={onBookmarkedChange}
             type="checkbox"
-            checked={ bookmarkedOnly }
+            checked={bookmarkedOnly}
           />
 
           <label
             data-testid="select-input-label"
             htmlFor="select-gender"
-            value={ selectedGenre }
+            value={selectedGenre}
           >Filtrar por gênero
           </label>
 
           <select
             id="select-gender"
             data-testid="select-input"
-            onChange={ onSelectedGenreChange }
-            value={ selectedGenre }>
-              <option
-                data-testid="select-option"
-                value=""
-              >Todos</option>
-              <option
-                data-testid="select-option" 
-                value="action"
-              >Ação</option>
-              <option
-                data-testid="select-option" 
-                value="comedy"
-              >Comédia</option>
-              <option
-                data-testid="select-option" 
-                value="thriller"
-              >Suspense</option>
+            onChange={onSelectedGenreChange}
+            value={selectedGenre}
+          >
+            <option
+              data-testid="select-option"
+              value=""
+            >Todos</option>
+            <option
+              data-testid="select-option"
+              value="action"
+            >Ação</option>
+            <option
+              data-testid="select-option"
+              value="comedy"
+            >Comédia</option>
+            <option
+              data-testid="select-option"
+              value="thriller"
+            >Suspense</option>
           </select>
         </form>
       </div>
