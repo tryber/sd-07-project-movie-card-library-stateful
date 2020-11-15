52330a1fcda8a140e1489fae528b9c22bad0b509
sttefanieMaciel <macielsttefanie@gmail.com>
2020-11-15 16:45:34 -0300

    Criado o componente searchBar

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,10 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  render() {
+    return (
+        <h2>AddMovie</h2>
+    );
+  }
+}
+export default AddMovie;
