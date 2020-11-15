3cdfdaf9e891d1441aee2b683623fc6795bf7807
Lucas Andrade <lafdesouza2002@outlook.com>
2020-11-15 05:21:11 -0300

    Corrigindo erro

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -23,6 +23,7 @@ export default class MovieLibrary extends React.Component {
     };
   }
   render() {
+    const { movies } = this.props;
     return (
       <div>
       <SearchBar
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,6 +4,14 @@ import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
     return (
       <div>
         <form action="" data-testid="search-bar-form">
