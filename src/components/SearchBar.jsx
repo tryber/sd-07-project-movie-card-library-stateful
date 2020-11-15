256ac9fe105d8d5089edde7fef9ff0a7d1b9f440
sttefanieMaciel <macielsttefanie@gmail.com>
2020-11-15 17:22:27 -0300

    criando os props

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
+    const {
+        searchText,
+        onSearchTextChange,
+        bookmarkedOnly,
+        onBookmarkedChange,
+        selectedGenre,
+        onSelectedGenreChange,
+    } = this.props;
     return (
       <header>
         <h1>SearchBar</h1>
@@ -9,4 +18,12 @@ class SearchBar extends React.Component {
     );
   }
 }
+SearchBar.propTypes = {
+    SearchBar: PropTypes.string.isRequired,
+    onSearchTextChange: PropTypes.func.isRequired,
+    bookmarkedOnly: PropTypes.bool.isRequired,
+    onBookmarkedChange: PropTypes.func.isRequired,
+    selectedGenre: PropTypes.string.isRequired,
+    onSelectedGenreChange: PropTypes.func.isRequired,
+};
 export default SearchBar;
