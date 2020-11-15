1d68d94e646da4cfdb538af06f01ff88b9abec55
TIAGO <tiagoesdrasvieira@gmail.com>
2020-11-15 20:37:57 -0300

    Renderize MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import AddMovie from './AddMovie';
+import SearchBar from './SearchBar';
 
 class MovieLibrary extends React.Component {
   render() {
-    return (<div />);
+    const { movies } = this.props;
+    console.log(movies);
+    return (
+      <div>
+        <SearchBar />
+        <AddMovie />
+      </div>
+    );
   }
 }
 
+MovieLibrary.propTypes = { movies: PropTypes.arrayOf(PropTypes.object).isRequired };
 export default MovieLibrary;
-
