74d1a1b3a8ef1726c50900b5d0bff5dfde922f6c
Quintiliano Dalete <quintilianodalete@gmail.com>
2020-11-15 08:47:13 -0300

    Creating a SearchBar component, redering a form in the SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,21 @@
 // implement SearchBar component here
+
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+
+    return(
+      <div>
+        <form data-testid="search-bar-form">
+          <label data-testid="text-input-label" htmlFor="search-input">Inclui o texto</label>
+          <input data-testid="text-input" onChange={onSearchTextChange} value={searchText}></input>
+        </form>
+      </div>
+    )
+  }
+}
+
+
+export default SearchBar;
