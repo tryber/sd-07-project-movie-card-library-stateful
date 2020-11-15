7ef22bc80f23379b58d7613378cdc8f6981e9889
Lucas Andrade <lafdesouza2002@outlook.com>
2020-11-15 03:18:16 -0300

    Criando componentes

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,9 +2,21 @@
 import React from 'react';
 
 export default class SearchBar extends React.Component {
-  render() {
-    return (
-      <div />
-    );
-  }
+	render() {
+		return (
+			<div>
+				<form action='' data-testid='search-bar-form'>
+					<label htmlFor=''>
+						<input
+							type='text'
+							name=''
+							value={this.props.searchText}
+							onChange={this.props.onSearchTextChange}
+							id=''
+						/>
+					</label>
+				</form>
+			</div>
+		);
+	}
 }
