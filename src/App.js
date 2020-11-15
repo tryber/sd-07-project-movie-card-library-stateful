32f172d3eadb7c07d87282b08ed05c0584338be8
Alstent <rafaelasltent@gmail.com>
2020-11-15 17:35:41 +0100

    iniciando movie card library stateful project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
-
 import Header from './components/Header';
+import SearchBar from './components/SearchBar';
+import MovieData from './data';
+import MovieList from './components/MovieList';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar 
+        searchText='uma string'
+        onSearchTextChange='uma callback'
+        bookmarkedOnly='um boolean'
+        onBookmarkedChange='uma callback'
+        selectedGenre='uma string'
+        onSelectedGenreChange='uma callback'
+      />
+      <MovieList movies={MovieData} />
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,20 @@
 // implement SearchBar component here
+import React from 'react'
+
+export class SearchBar extends React.Component {
+  render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props
+    return (
+      <div>
+        <label data-testid="text-input-label">Inclui o texto:</label>
+        <div>
+          <input type="text" name="text"
+          value={searchText} 
+          onChange={onSearchTextChange} />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default SearchBar
