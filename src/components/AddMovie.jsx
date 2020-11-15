2cc802e14a103eaaa8cbf9df8405d41291ffde30
Quintiliano Dalete <quintilianodalete@gmail.com>
2020-11-15 13:04:15 -0300

    Ajusting code climate

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -21,7 +21,7 @@ class AddMovie extends Component {
 
   changeStateValue(target) {
     const { name, value } = target;
-    this.setState({[name]: value});
+    this.setState({ [name]: value });
   }
 
   addMovieForm() {
@@ -39,27 +39,58 @@ class AddMovie extends Component {
   }
 
   render() {
-    //const { onClick } = this.props;
+    // const { onClick } = this.props;
     return (
       <form data-testid="add-movie-form">
         <label data-testid="title-input-label">Título</label>
-        <input name="title" data-testid="title-input" onChange={this.changeStateValue} value={this.state.title} />
+        <input
+          name="title"
+          data-testid="title-input"
+          onChange={this.changeStateValue}
+          value={this.state.title}
+        />
 
         <label data-testid="subtitle-input-label">Subtítulo</label>
-        <input name="subtitle" data-testid="subtitle-input" value={this.state.subtitle} onChange={this.changeStateValue} />
+        <input
+          name="subtitle"
+          data-testid="subtitle-input"
+          value={this.state.subtitle}
+          onChange={this.changeStateValue}
+        />
 
         <label data-testid="image-input-label">Imagem</label>
-        <input name="imagePath" data-testid="image-input" onChange={this.changeStateValue} value={this.state.imagePath} />
+        <input
+          name="imagePath"
+          data-testid="image-input"
+          onChange={this.changeStateValue}
+          value={this.state.imagePath}
+        />
 
         <label data-testid="storyline-input-label">Sinopse</label>
-        <textarea name="storyline" data-testid="storyline-input" onChange={this.changeStateValue} value={this.state.storyline} />
+        <textarea
+          name="storyline"
+          data-testid="storyline-input"
+          onChange={this.changeStateValue}
+          value={this.state.storyline}
+        />
 
 
         <label data-testid="rating-input-label">Avaliação</label>
-        <input name='rating' value={this.state.rating} data-testid="rating-input" onChange={this.changeStateValue} type="number" />
+        <input
+          name='rating'
+          value={this.state.rating}
+          data-testid="rating-input"
+          onChange={this.changeStateValue}
+          type="number"
+        />
 
         <label data-testid="genre-input-label">Gênero</label>
-        <select name='genre' value={this.state.genre} data-testid="genre-input" onChange={this.changeStateValue} >
+        <select
+          name='genre'
+          value={this.state.genre}
+          data-testid="genre-input"
+          onChange={this.changeStateValue}
+        >
           <option data-testid="genre-option" value='action'>Ação</option>
           <option data-testid="genre-option" value='comedy'>Comédia</option>
           <option data-testid="genre-option" value='thriller'>Suspense</option>
