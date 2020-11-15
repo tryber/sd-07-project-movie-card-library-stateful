23a6f8c1b09cbf89566ef247d2fa6cedbdc81ff2
Lucas Andrade <lafdesouza2002@outlook.com>
2020-11-15 03:26:05 -0300

    Criando componentes

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,11 @@
 // implement SearchBar component here
-import React from "react";
+import React from 'react';
 
 export default class SearchBar extends React.Component {
   render() {
     return (
       <div>
-        <form action="" data-testid="search-bar-form">
+        <form action='' data-testid="search-bar-form">
           <label htmlFor="">
             <input
               type="text"
