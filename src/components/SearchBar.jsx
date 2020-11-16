29a3de02fc20cc0fba318c1b12a68cb01709eec1
Raphael Caputo <faelelf2@gmail.com>
2020-11-16 18:11:54 -0300

    Chegando ate o requisito input CheckBox- começando ele agora

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,18 @@
 // implement SearchBar component here
 import React, { Component } from 'react';
-
+import PropTypes from 'propTypes';
 
 class SearchBar extends Component {
   render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
     return (
-     <form>
-       
-     </form>
+    <form data-testid='search-bar-form'>
+      <label htmlFor='text-input' data-testid='text-input-label'>
+        Inclui o texto
+      <input id='text-input' data-testid='text-input' type='text' name='searchText'
+      value={searchText} onChange={onSearchTextChange} />
+    </label>
+    </form>
     )
   }
 }
