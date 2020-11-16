df39d9d9d5185788daa661a2b1d4f7acfbf5e8e5
Raphael Caputo <faelelf2@gmail.com>
2020-11-16 17:41:12 -0300

    Requisito 2

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,15 @@
 // implement SearchBar component here
+import React, { Component } from 'react';
+
+
+class SearchBar extends Component {
+  render() {
+    return (
+     <form>
+       
+     </form>
+    )
+  }
+}
+
+export default SearchBar;
