a62cb5f9c59be5ec29a656d384b944951d2faed7
vitorAmorims <vtamorims@gmail.com>
2020-11-16 14:24:01 -0300

    Projeto completo

---
+++ src/components/RatingFormulario.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Rating extends Component {
+  render() {
+    const { value, handleChange } = this.props;
+    return (
+      <label htmlFor={'rating-input'} data-testid="rating-input-label">
+        Avaliação
+        <input
+          name="rating"
+          type="number"
+          value={value}
+          data-testid="rating-input"
+          className="movie-card-rating"
+          onChange={handleChange}
+        />
+      </label>
+    );
+  }
+}
+
+Rating.protoTypes = {
+  value: PropTypes.number.isRequired,
+  handleChange: PropTypes.func
+};
+
+export default Rating;
---
+++ src/components/Subtitle.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Subtitle extends Component {
+  render() {
+    const { value, handleChange } = this.props;
+    return (
+      <label htmlFor={'subtitle-input'} data-testid="subtitle-input-label">
+        Subtítulo
+        <input
+          name="subtitle"
+          type="text"
+          value={value}
+          data-testid="subtitle-input"
+          onChange={handleChange}
+        />
+      </label>
+    );
+  }
+}
+Subtitle.propTypes = {
+  value: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired
+};
+
+export default Subtitle;
