d9a7c8b905a8de84faad4f715f86cf2699124041
Leonardo <lecastro16@gmail.com>
2020-11-16 14:36:17 -0300

    iniciando v1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,24 @@
-// implement SearchBar component here
+import React from "react";
+// import propTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick=this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    console.log(this);
+    console.log("clicou!");
+  }
+  render() {
+    return (
+      <div>
+        <h1 className="search-bar">Searching</h1>
+        <button onClick={this.handleClick}>Meu Botao</button>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
