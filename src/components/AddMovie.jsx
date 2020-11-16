5174357662364a0cf2c2ec092f7446f34aed9e0c
Vitor Rodrigues <vitor_rc@outlook.com>
2020-11-16 13:00:32 -0300

    Requerimentos restantes

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,11 +1,142 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.updateState = this.updateState.bind(this);
+    this.addMovie = this.addMovie.bind(this);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+  updateState({ target }) {
+    const value = target.name === 'rating' ? Number(target.value) : target.value;
+    this.setState({ [target.name]: value });
+  }
+
+  addMovie(callbackFunc) {
+    callbackFunc(this.state);
+    this.setState({
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    });
+  }
+
   render() {
     return (
-      <div>teste</div>
+      <form data-testid="add-movie-form">
+        <label
+          htmlFor="title"
+          data-testid="title-input-label"
+        >
+          Título
+        </label>
+        <input
+          type="text"
+          name="title"
+          value={this.state.title}
+          data-testid="title-input"
+          onChange={this.updateState}
+        />
+
+        <label
+          htmlFor="subtitle"
+          data-testid="subtitle-input-label"
+        >
+          Subtítulo
+        </label>
+        <input
+          type="text"
+          name="subtitle"
+          value={this.state.subtitle}
+          data-testid="subtitle-input"
+          onChange={this.updateState}
+        />
+
+        <label
+          htmlFor="imagePath"
+          data-testid="image-input-label"
+        >
+          Imagem
+        </label>
+        <input
+          type="text"
+          name="imagePath"
+          value={this.state.imagePath}
+          data-testid="image-input"
+          onChange={this.updateState}
+        />
+
+        <label
+          htmlFor="storyline"
+          data-testid="storyline-input-label"
+        >
+          Sinopse
+        </label>
+        <textarea
+          type="text"
+          name="storyline"
+          value={this.state.storyline}
+          data-testid="storyline-input"
+          onChange={this.updateState}
+        />
+
+        <label
+          htmlFor="rating"
+          data-testid="rating-input-label"
+        >
+          Avaliação
+        </label>
+        <input
+          type="number"
+          name="rating"
+          value={this.state.rating}
+          data-testid="rating-input"
+          onChange={this.updateState}
+        />
+
+        <label
+          htmlFor="genre"
+          data-testid="genre-input-label"
+        >
+          Gênero
+        </label>
+        <select
+          name="genre"
+          data-testid="genre-input"
+          value={this.state.genre}
+          onChange={this.updateState}
+        >
+          <option data-testid="genre-option" value="action">Ação</option>
+          <option data-testid="genre-option" value="comedy">Comédia</option>
+          <option data-testid="genre-option" value="thriller">Suspense</option>
+        </select>
+
+        <button
+          data-testid="send-button"
+          onClick={(event) => {
+            event.preventDefault();
+            this.addMovie(this.props.onClick);
+          }}
+        >
+          Adicionar filme
+        </button>
+      </form>
     );
   }
 }
+
+AddMovie.propTypes = { onClick: PropTypes.func.isRequired };
 
 export default AddMovie;
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,78 @@
-// implement MovieLibrary component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
+    };
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+    this.addMovie = this.addMovie.bind(this);
+  }
+  onSearchTextChange({ target }) {
+    const { value } = target;
+    this.setState({ searchText: value }, this.moviesFilter);
+  }
+  onBookmarkedChange({ target }) {
+    const { checked } = target;
+    this.setState({ bookmarkedOnly: checked }, this.moviesFilter);
+  }
+  onSelectedGenreChange({ target }) {
+    const { value } = target;
+    this.setState({ selectedGenre: value }, this.moviesFilter);
+  }
+
+  moviesFilter() {
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+    let filteredMovies = [];
+
+    filteredMovies = this.props.movies.filter((movie) =>
+      movie.title.includes(searchText) ||
+      movie.subtitle.includes(searchText) ||
+      movie.storyline.includes(searchText),
+    );
+    if (bookmarkedOnly) {
+      filteredMovies = filteredMovies.filter((movie) => movie.bookmarked === true);
+    }
+
+    if (selectedGenre !== '') {
+      filteredMovies = filteredMovies.filter((movie) => movie.genre === selectedGenre);
+    }
+
+    this.setState({ movies: filteredMovies });
+  }
+
+  addMovie(newMovie) {
+    this.setState({ movies: [...this.state.movies, newMovie] });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar
+          searchText={this.state.searchText}
+          onSearchTextChange={this.onSearchTextChange}
+          bookmarkedOnly={this.state.bookmarkedOnly}
+          onBookmarkedChange={this.onBookmarkedChange}
+          selectedGenre={this.state.selectedGenre}
+          onSelectedGenreChange={this.onSelectedGenreChange}
+        />
+        <MovieList movies={this.state.movies} />
+        <AddMovie onClick={this.addMovie} />
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = { movies: PropTypes.arrayOf(PropTypes.object).isRequired };
+
+export default MovieLibrary;
