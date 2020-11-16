a62d7add8d1dd3f45130f29fc1ec320d8e06b1f1
Mateus Leite Alves de Almeida <mateuslaalmeida@gmail.com>
2020-11-15 23:20:43 -0300

    Alteraçoes para o terceiro requisito

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -13,6 +13,10 @@ class SearchBar extends React.Component {
           <label data-testid="text-input-label"> Inclui o texto
           <input value={this.props.searchText} onChange={this.props.onSearchTextChange} data-testid="text-input"/>
           </label>
+
+          <label data-testid="checkbox-input-label"> Mostrar somente favoritos
+          <input type="checkbox" checked={this.props.bookmarkedOnly} onChange={this.props.onBookmarkedChange} data-testid="checkbox-input"/>
+          </label>
         </form>
       </div>
     );
