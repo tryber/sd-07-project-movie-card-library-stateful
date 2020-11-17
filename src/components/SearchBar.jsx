e1904ed5cf552db9414feeddcba3ad368f20b9ac
Marcos R. Silva <marcos.1998226@gmail.com>
2020-11-17 00:22:35 -0300

    finalizando a implementaçao do serchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,39 +4,45 @@ import PropTypes from 'prop-types';
 class SearchBar extends React.Component {
   render() {
     return (
-      <form data-testid="search-bar-form">
-        <label data-testid="text-input-label" htmlFor="search">
+      <div className="myForm">
+        <form data-testid="search-bar-form">
+          <label data-testid="text-input-label" htmlFor="search">
             Inclui o texto <br />
-          <input
-            type="text"
-            name="search"
-            data-testid="text-input"
-            value={this.props.searchText} onChange={this.props.onSearchTextChange}
-          />
-        </label>
-        <br />
-        <label data-testid="checkbox-input-label" htmlFor="checkBox">
-          Mostrar somente favoritos <br />
-          <input
-            data-testid="checkbox-input"
-            type="checkbox"
-            checked={this.props.bookmarkedOnly}
-            onChange={this.props.onBookmarkedChange}
-            name="checkBox"
-          />
-        </label>
-        <br />
-        <label data-testid="select-input-label" htmlFor="movieGenre">
-            Filtrar por gênero <br />
-          <select
-            data-testid="select-input"
-            onChange={this.props.onSelectedGenreChange}
-            name="movieGenre"
-          >
-            <option value={this.props.selectedGenre} />
-          </select>
-        </label>
-      </form>
+            <input
+              type="text"
+              name="search"
+              data-testid="text-input"
+              value={this.props.searchText} onChange={this.props.onSearchTextChange}
+            />
+          </label>
+          <br />
+          <label data-testid="checkbox-input-label" htmlFor="checkBox">
+            Mostrar somente favoritos <br />
+            <input
+              data-testid="checkbox-input"
+              type="checkbox"
+              checked={this.props.bookmarkedOnly}
+              onChange={this.props.onBookmarkedChange}
+              name="checkBox"
+            />
+          </label>
+          <br />
+          <label data-testid="select-input-label" htmlFor="movieGenre">
+              Filtrar por gênero <br />
+            <select
+              data-testid="select-input"
+              value={this.props.selectedGenre}
+              onChange={this.props.onSelectedGenreChange}
+              name="movieGenre"
+            >
+              <option data-testid="select-option" value="">Todos</option>
+              <option data-testid="select-option" value="action">Ação</option>
+              <option data-testid="select-option" value="comedy">Comédia</option>
+              <option data-testid="select-option" value="thriller">Suspense</option>
+            </select>
+          </label>
+        </form>
+      </div>
     );
   }
 }
