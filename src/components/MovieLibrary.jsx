58232cf77e901ac33cbd90d6a6e966cd1421b68b
Lucas Andrade <lafdesouza2002@outlook.com>
2020-11-17 12:37:20 -0300

    Corrigindo issues

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
-import movies from '../data';
 
 class MovieLibrary extends React.Component {
   constructor(props) {
@@ -12,31 +11,29 @@ class MovieLibrary extends React.Component {
     this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
     this.onSearchTextChange = this.onSearchTextChange.bind(this);
     this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
-    this.filterMovie = this.filterMovie.bind(this)
+    this.filterMovie = this.filterMovie.bind(this);
     this.state = {
       searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
-      movies: props.movies
+      movies: props.movies,
     };
   }
 
-  onBookmarkedChange({target}) {
-    this.setState({
-      bookmarkedOnly: target.type === 'checkbox' ? target.checked : target.value
-    })
+  onBookmarkedChange({ target }) {
+    this.setState({ bookmarkedOnly: target.type === 'checkbox' ? target.checked : target.value, });
   }
 
-  onSearchTextChange({search}) {
-    this.setState({searchText: search})
+  onSearchTextChange({ search }) {
+    this.setState({searchText: search});
   }
 
-  onSelectedGenreChange({option}) {
-    this.setState({selectedGenre: option})
+  onSelectedGenreChange({ option }) {
+    this.setState({selectedGenre: option});
   }
 
   filterMovie() {
-      return this.props.movies.filter(movie => movie.title.includes(this.state.searchText))
+    return this.props.movies.filter((movie) => movie.title.includes(this.state.searchText));
   }
   render() {
     // const { movies } = this.props;
