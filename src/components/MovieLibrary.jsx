3ce491bcb6e478f7f54194c2248b275c14ebfb92
Renato Silva <renatosdev@gmail.com>
2020-11-17 19:10:11 -0300

    Requisitos MovieLibraty - inicio

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,33 @@
-// implement MovieLibrary component here
+import React from 'react';
+import SearchBar from './SearchBar';
+import PropTypes from 'prop-types';
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+    };
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar
+          searchText={this.props.searchText}
+          bookmarkedOnly={this.props.bookmarkedOnly}
+          selectedGenre={this.props.selectedGenre}
+        />
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+};
+
+export default MovieLibrary;
