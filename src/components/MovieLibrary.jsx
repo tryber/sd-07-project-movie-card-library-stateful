29098ce684efc0fedd768fdb3d29887e7e361f56
Iury Nathan <iurynathan@gmail.com>
2020-11-17 16:17:05 -0300

    code climate

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -42,7 +42,9 @@ class MovieLibrary extends React.Component {
       storyline.toLowerCase().includes(this.state.searchText.toLowerCase()))
       .filter(({ genre }) => genre.includes(this.state.selectedGenre))
       // Este restante da lógica feita com ajuda de Thaydds :D Obrigado.
-      .filter(({ bookmarked }) => this.state.bookmarkedOnly ? bookmarked : true );
+      .filter(function ({ bookmarked }) {
+        this.state.bookmarkedOnly ? bookmarked : true;
+      });
     return result;
   }
 
