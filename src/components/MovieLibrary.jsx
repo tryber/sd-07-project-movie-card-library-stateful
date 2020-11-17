f4f2a73bd6cf8867bd0ae29089a199b5c159a0c2
Renato Silva <renatosdev@gmail.com>
2020-11-17 19:12:35 -0300

    Requisitos MovieLibraty - inicio - cc

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import SearchBar from './SearchBar';
 import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
 
 class MovieLibrary extends React.Component {
   constructor(props) {
