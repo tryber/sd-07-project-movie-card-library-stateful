9d3f31e2b27f1ff73aa71acef730276b81ea94b7
Lucas Andrade <lafdesouza2002@outlook.com>
2020-11-17 00:55:18 -0300

    Corrigindo issue

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -22,36 +22,37 @@ export default class AddMovie extends React.Component {
             Título
           </label>
           <input type="text" name="" id="addTitle" data-testid="title-input" />
-          <label htmlFor="addTitle" data-testid="subtitle-input-label">
+          <label htmlFor="addSubtitle" data-testid="subtitle-input-label">
             Subtítulo
           </label>
           <input
             type="text"
             name=""
-            id="addTitle"
+            id="addSubtitle"
             data-testid="subtitle-input"
           />
-          <label htmlFor="addTitle" data-testid="image-input-label">
+          <label htmlFor="addImage" data-testid="image-input-label">
             Imagem
           </label>
-          <input type="text" name="" id="addTitle" data-testid="image-input" />
-          <label htmlFor="addTitle" data-testid="storyline-input-label">
+          <input type="text" name="" id="addImage" data-testid="image-input" />
+          <label htmlFor="addStoryline" data-testid="storyline-input-label">
             Sinopse
           </label>
-          <textarea name=""
-            id="addTitle"
-            data-testid="storyline-input" 
-            cols="30" 
-            rows="10">
-            </textarea>
-          <label htmlFor="addTitle" data-testid="rating-input-label">
+          <textarea 
+            name=""
+            id="addStoryline"
+            data-testid="storyline-input"
+            cols="30"
+            rows="10" 
+          />
+          <label htmlFor="addRating" data-testid="rating-input-label">
             Avaliação
           </label>
-          <input type="text" name="" id="addTitle" data-testid="rating-input" />
-          <label htmlFor="addTitle" data-testid="genre-input-label">
+          <input type="text" name="" id="addRating" data-testid="rating-input" />
+          <label htmlFor="addGender" data-testid="genre-input-label">
             Gênero
           </label>
-          <select name="" id="addTitle" data-testid="genre-option">
+          <select name="" id="addGender" data-testid="genre-option">
             <option value="action">Ação</option>
             <option value="comedy">Comédia</option>
             <option value="thriller">Suspense</option>
