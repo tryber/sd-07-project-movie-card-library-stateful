2aeb1448ba131e88f1f1b1c1b4f466807922d30e
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2020-11-16 23:28:55 -0300

    Perdido nas importações

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import MovieList from './components/MovieList';
 import Header from './components/Header';
 import movies from './data';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar />
       <MovieList movies={movies} />
     </div>
   );
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,10 +5,11 @@ import MovieCard from './MovieCard';
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
-
     return (
       <div data-testid="movie-list" className="movie-list">
-        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+        {movies.map((movie) => (
+          <MovieCard key={movie.title} movie={movie} />
+        ))}
       </div>
     );
   }
