99cf214d79bf00ea8a1ea557551fca5d632e9892
vitorAmorims <vtamorims@gmail.com>
2020-11-17 12:51:00 -0300

    code climate

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
@@ -11,8 +11,6 @@ class Rating extends React.Component {
     );
   }
 }
-Rating.propTypes = {
-  rating: PropTypes.number.isRequired,
-}
+Rating.propTypes = { rating: PropTypes.number.isRequired, };
 
 export default Rating;
