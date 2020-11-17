ec400e8e7e87fd81f766c86180fe240ecc363aee
Raphael Rodrigues <rapha.rodrigues04@gmail.com>
2020-11-17 20:48:03 -0300

    Ajustando inconsistencias no AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,121 +1,117 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const stateInicial = {
-  subtitle: '',
-  title: '',
-  imagePath: '',
-  storyline: '',
-  rating: 0,
-  genre: 'action',
-};
-
 class AddMovie extends React.Component {
   constructor() {
     super();
-    this.state = stateInicial;
+
+    this.getHandler = this.getHandler.bind(this);
+    this.addMovie = this.addMovie.bind(this);
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
   }
 
-  HandleChange({ target }) {
+  getHandler({ target }) {
     const { name, value } = target;
-    this.setState({ [name]: value });
+    this.setState(() => ({ [name]: value }));
   }
 
-  addNewMovie(callback) {
-    callback(this.state);
-    this.setState(stateInicial);
+  addMovie(event) {
+    event.preventDefault();
+    const { onClick } = this.props;
+
+
+    this.setState((previousState) => {
+      onClick(previousState);
+      return ({
+        subtitle: '',
+        title: '',
+        imagePath: '',
+        storyline: '',
+        rating: 0,
+        genre: 'action',
+      });
+    });
   }
 
   render() {
-    const { onClick } = this.props;
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
 
     return (
-      <form className="form-add-movie">
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label" htmlFor="title">Título
+        <input
+          data-testid="title-input"
+          type="text"
+          name="title"
+          value={title}
+          onChange={this.getHandler}
+        />
+        </label>
 
-        <div>
-          <label data-testid="title-input-label" htmlFor="titulo">Título</label>
-          <input
-            data-testid="title-input"
-            type="text"
-            name="title"
-            value={this.state.title}
-            onChange={this.HandleChange}
-            id="titulo"
-          />
-        </div>
+        <label data-testid="subtitle-input-label" htmlFor="subtitle">Subtítulo
+        <input
+          data-testid="subtitle-input"
+          type="text"
+          name="subtitle"
+          value={subtitle}
+          onChange={this.getHandler}
+        />
+        </label>
 
-        <div>
-          <label data-testid="subtitle-input-label" htmlFor="subtitulo">Subtítulo</label>
-          <input
-            data-testid="subtitle-input"
-            type="text"
-            name="subtitle"
-            value={this.state.subtitle}
-            onChange={this.HandleChange}
-            id="subtitulo"
-          />
-        </div>
+        <label data-testid="image-input-label" htmlFor="imagePath">Imagem
+        <input
+          data-testid="image-input"
+          type="text"
+          name="imagePath"
+          value={imagePath}
+          onChange={this.getHandler}
+        />
+        </label>
 
-        <div>
-          <label data-testid="image-input-label" htmlFor="imagem">Imagem</label>
-          <input
-            data-testid="image-input"
-            type="text"
-            name="imagePath"
-            value={this.state.imagePath}
-            onChange={this.HandleChange}
-            id="imagem"
-          />
-        </div>
+        <label data-testid="storyline-input-label" htmlFor="storyline">Sinopse
+        <textarea
+          data-testid="storyline-input"
+          name="storyline"
+          value={storyline}
+          onChange={this.getHandler}
+        />
+        </label>
 
-        <div>
-          <label data-testid="storyline-input-label" htmlFor="sinpse">Sinopse</label>
-          <textarea
-            data-testid="storyline-input"
-            name="storyline"
-            value={this.state.storyline}
-            onChange={this.HandleChange}
-            id="imagem"
-          />
-        </div>
+        <label data-testid="rating-input-label" htmlFor="rating">Avaliação
+        <input
+          data-testid="rating-input"
+          type="number"
+          name="rating"
+          value={rating}
+          onChange={this.getHandler}
+        />
+        </label>
 
-        <div>
-          <label data-testid="rating-input-label" htmlFor="avaliacao">Avaliação</label>
-          <input
-            data-testid="rating-input"
-            type="number"
-            name="rating"
-            value={this.state.rating}
-            onChange={this.HandleChange}
-            id="avaliacao"
-          />
-        </div>
+        <label data-testid="genre-input-label" htmlFor="genre">Gênero
+        <select
+          data-testid="genre-input"
+          name="genre"
+          value={genre}
+          onChange={this.getHandler}
+        >
+          <option data-testid="genre-option" value="action">Ação</option>
+          <option data-testid="genre-option" value="comedy">Comédia</option>
+          <option data-testid="genre-option" value="thriller">Suspense</option>
+        </select>
+        </label>
 
-        <div>
-          <label data-testid="genre-input-label" htmlFor="genero">Gênero</label>
-          <select
-            data-testid="genre-input"
-            name="genre"
-            value={this.state.genre}
-            onChange={this.HandleChange}
-            htmlFor="genero"
-          >
-            <option data-testid="genre-option" value="action">Ação</option>
-            <option data-testid="genre-option" value="comedy">Comédia</option>
-            <option data-testid="genre-option" value="thriller">Suspense</option>
-          </select>
-        </div>
-
-        <div>
-          <button
-            type="submit"
-            data-testid="send-button"
-            onClick={(item) => {
-              item.preventDefault();
-              this.addNewMovie(onClick);
-            }}
-          >Adicionar filme
-          </button>
+        <div className="button-addMovie">
+          <button data-testid="send-button" onClick={this.addMovie}>
+            Adicionar filme
+        </button>
         </div>
 
       </form>
