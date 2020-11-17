90034a072802d89fb4a58f07fce76782aa77f8da
Raphael Caputo <faelelf2@gmail.com>
2020-11-17 02:09:22 -0300

    Tentando chegar aos 80

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import SearchBar from './components/SearchBar';
+import movie from './data'
 import './App.css';
 
 import Header from './components/Header';
+import MovieLibrary from './components/MovieLibrary';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <SearchBar />
+      <MovieLibrary movies={movie}
+       />
     </div>
   );
 }
