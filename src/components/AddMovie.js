07690ce8ca702104ca226b55de4d9449ca24360b
vitorAmorims <vtamorims@gmail.com>
2020-11-17 10:05:59 -0300

    code climate

--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -22,7 +22,7 @@ class AddMovie extends Component {
     };
   }
 
-  handleChange = (event) => {
+  handleChange(event) {
     this.setState({
       [event.target.name]: event.target.value,
     });
