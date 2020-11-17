5097d6d0da19b6f920ce4dbf0726395ebcb188f8
TIAGO <tiagoesdrasvieira@gmail.com>
2020-11-17 13:44:02 -0300

    Renderize MovieLibrary - tentando

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import AddMovie from './AddMovie';
 import SearchBar from './SearchBar';
-import movies from '../data';
 import MovieList from './MovieList';
 
 class MovieLibrary extends React.Component {
 
   constructor(props) {
     super(props);
+    const { movies } = this.props;
     this.onSearchTextChange = this.onSearchTextChange.bind(this);
     this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
     this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
@@ -42,7 +42,7 @@ class MovieLibrary extends React.Component {
           onBookmarkedChange={this.onBookmarkedChange}
           onSelectedGenreChange={this.onSelectedGenreChange}
         />
-        <MovieList movies={movies} />
+        <MovieList movies={this.state.movies} />
         <AddMovie />
       </div>
     );
