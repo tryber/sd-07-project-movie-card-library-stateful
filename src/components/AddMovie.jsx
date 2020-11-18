ede90c931220b3dfaee9d919f9bd1b76bcb6ada5
Eduardo Pizorno <pizorno@gmail.com>
2020-11-18 09:51:24 -0300

    CodeClimate AddMovie 0.0.1

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 class AddMovie extends React.Component {
   render() {
     return (
-        <div>
-          <h1>ADD MOVIE</h1>
-        </div>
-      );
+      <div>
+        <h1>ADD MOVIE</h1>
+      </div>
+    );
   }
-  }
+}
 export default AddMovie;
