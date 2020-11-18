13bb34ec9f3d18acced7a6a5c9b86dc3cc1240dc
Lucas Ribeiro <martins7ribeiro@gmail.com>
2020-11-18 00:50:18 -0300

    do zero ao topo: primeiro commit

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,119 @@
 // implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+
+    this.initialState = Object.assign(this.state);
+
+    this.newMovieHandler = this.newMovieHandler.bind(this);
+    this.newMovieSubtitleHandler = this.newMovieSubtitleHandler.bind(this);
+    this.newMovieImageHandler = this.newMovieImageHandler.bind(this);
+    this.newMovieStorylineHandler = this.newMovieStorylineHandler.bind(this);
+    this.newMovieRatingHandler = this.newMovieRatingHandler.bind(this);
+    this.newMovieGenreHandler = this.newMovieGenreHandler.bind(this);
+    this.onClick = this.onClick.bind(this);
+  }
+// ------------------------------------------------------------------------
+  newMovieHandler(event) {
+    this.setState({ title: event.target.value });
+  }
+
+  newMovieSubtitleHandler(event) {
+    this.setState({ subtitle: event.target.value });
+  }
+
+  newMovieImageHandler(event) {
+    this.setState({ imagePath: event.target.value });
+  }
+
+  newMovieStorylineHandler(event) {
+    this.setState({ storyline: event.target.value });
+  }
+
+  newMovieRatingHandler(event) {
+    this.setState({ rating: event.target.value });
+  }
+
+  newMovieGenreHandler(event) {
+    this.setState({ genre: event.target.value });
+  }
+
+  onClick(state) {
+    // const actualState = state
+    this.setState(this.initialState);
+  }
+// ------------------------------------------------------------------------  
+  render () {
+    const { onClick } = this.props;
+    return (
+      <form action="">
+        <label htmlFor="new-movie-title" data-testid="title-input-label">Título</label>
+        <input 
+          type="text"
+          value={this.state.title}
+          data-testid="title-input"
+          onChange={this.newMovieHandler}
+        />
+
+        <label htmlFor="new-movie-subtitle" data-testid="subtitle-input-label">Subtítulo</label>
+        <input 
+          type="text"
+          value={this.state.subtitle}
+          data-testid="subtitle-input"
+          onChange={this.newMovieSubtitleHandler}
+        />
+
+        <label htmlFor="new-movie-image" data-testid="image-input-label">Imagem</label>
+        <input 
+          type="text"
+          value={this.state.imagePath}
+          data-testid="image-input"
+          onChange={this.newMovieImageHandler}
+        />
+
+        <label htmlFor="new-movie-storyline" data-testid="storyline-input-label">Sinopse</label>
+        <input 
+          type="text"
+          data-testid="storyline-input"
+          value={this.state.storyline}
+          onChange={this.newMovieStorylineHandler}
+        />
+
+        <label htmlFor="new-movie-rating" data-testid="rating-input-label">Avaliação</label>
+        <input 
+          type="number"
+          value={this.state.rating}
+          data-testid="rating-input"
+          onChange={this.newMovieRatingHandler}
+        />
+
+        <label htmlFor="new-movie-genre" data-testid="genre-input-label">Gênero</label>
+        <select 
+          name="new-movie-genre" 
+          id="new-movie-genre"
+          value={this.state.genre}
+          data-testid="genre-input"
+          onChange={this.newMovieGenreHandler}
+        >
+          <option value="action" data-testid="genre-option">Ação</option>
+          <option value="comedy" data-testid="genre-option">Comédia</option>
+          <option value="thriller" data-testid="genre-option">Suspense</option>
+        </select>
+
+        <button data-testid="send-button" onClick={() => onClick(this.state)}>Adicionar filme</button>
+      </form>
+    )
+  }
+}
+
+export default AddMovie;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,60 @@
 // implement SearchBar component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  render() {
+    const { 
+      searchText, 
+      onSearchTextChange, 
+      bookmarkedOnly,
+      onBookmarkedChange, 
+      selectedGenre, 
+      onSelectedGenreChange 
+    } =  this.props;
+
+    return (
+      <form 
+        action=""
+        data-testid="search-bar-form"
+      >
+        <label htmlFor="search-text" data-testid="text-input-label">Inclui o texto:</label>
+        <input 
+          type="text"
+          name="search-text" 
+          id="search-text" 
+          value={searchText} 
+          onChange={onSearchTextChange}
+          data-testid="text-input"
+        />
+
+        <label htmlFor="bookmarked-only" data-testid="checkbox-input-label">Mostrar somente favoritos
+        <input 
+          type="checkbox" 
+          name="bookmarked-only" 
+          id="bookmarked-only"
+          checked={bookmarkedOnly}
+          onChange={onBookmarkedChange}
+          data-testid="checkbox-input"
+        />
+        </label>
+
+        <label htmlFor="selected-genre" data-testid="select-input-label">Filtrar por gênero</label>
+        <select 
+          name="selected-genre" 
+          id="selected-genre"
+          value={selectedGenre}
+          onChange={onSelectedGenreChange}
+          data-testid="select-input"
+        >
+          <option value="" data-testid="select-option">Todos</option>
+          <option value="action" data-testid="select-option">Ação</option>
+          <option value="comedy" data-testid="select-option">Comédia</option>
+          <option value="thriller" data-testid="select-option">Suspense</option>
+        </select>
+      </form>
+    )
+  }
+}
+
+export default SearchBar;
