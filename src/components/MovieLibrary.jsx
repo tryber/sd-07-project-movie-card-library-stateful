9003b91c5e4bb7114c74934eea36d4f55a4fba2a
Eduardo Pizorno <pizorno@gmail.com>
2020-11-18 09:09:38 -0300

    Adcionando formulario ao searchbar

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -12,7 +12,6 @@ class MovieLibrary extends Component {
   render() {
     return (
       <div>
-        <h2> My awesome movie library </h2>
         <SearchBar />
         <MovieList movies={this.props.movies} />
         <AddMovie />
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,17 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return(
+      <form data-testid="search-bar-form">
+        <label htmlFor="text-input">
+          <span>Inclui o texto: </span>
+          <input type="text" name="searchText" id="text-input" data-testid="text-input" />
+        </label>
+        <label htmlFor=""></label>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
