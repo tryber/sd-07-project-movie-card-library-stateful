7d631f53a2fc36162dfcc7cb735d47ab1c0e4192
Mateus Leite Alves de Almeida <mateuslaalmeida@gmail.com>
2020-11-18 01:01:47 -0300

    Alteraçoes para o decimo quinto requisito

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -19,75 +19,96 @@ class AddMovie extends React.Component {
     this.setState({ [name]: value });
   }
 
+  addOneMovie() {
+    this.setState({
+      title: '',
+      subtitle: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    });
+  }
+
   render() {
     return (
-      <form data-testid="add-movie-form">
+      <div>
+        <form data-testid="add-movie-form">
 
-        <label
-          htmlFor="title-input"
-          data-testid="title-input-label"
-          name="title"
-          value={this.state.title}
-          onChange={this.handleChange}
-        > Título
-          <input data-testid="title-input" />
-        </label>
+          <label
+            htmlFor="title-input"
+            data-testid="title-input-label"
+            name="title"
+            value={this.state.title}
+            onChange={this.handleChange}
+          > Título
+            <input data-testid="title-input" />
+          </label>
 
-        <label
-          htmlFor="subtitle-input"
-          data-testid="subtitle-input-label"
-          name="subtitle"
-          value={this.state.subtitle}
-          onChange={this.handleChange}
-        > Subtítulo
-          <input data-testid="subtitle-input" />
-        </label>
+          <label
+            htmlFor="subtitle-input"
+            data-testid="subtitle-input-label"
+            name="subtitle"
+            value={this.state.subtitle}
+            onChange={this.handleChange}
+          > Subtítulo
+            <input data-testid="subtitle-input" />
+          </label>
 
-        <label
-          htmlFor="image-input"
-          data-testid="image-input-label"
-          name="imagePath"
-          value={this.state.imagePath}
-          onChange={this.handleChange}
-        > Imagem
-          <input data-testid="image-input" />
-        </label>
+          <label
+            htmlFor="image-input"
+            data-testid="image-input-label"
+            name="imagePath"
+            value={this.state.imagePath}
+            onChange={this.handleChange}
+          > Imagem
+            <input data-testid="image-input" />
+          </label>
 
-        <label
-          htmlFor="storyline-input"
-          data-testid="storyline-input-label"
-          name="storyline"
-          value={this.state.storyline}
-          onChange={this.handleChange}
-        > Sinopse
-          <textarea data-testid="storyline-input" />
-        </label>
+          <label
+            htmlFor="storyline-input"
+            data-testid="storyline-input-label"
+            name="storyline"
+            value={this.state.storyline}
+            onChange={this.handleChange}
+          > Sinopse
+            <textarea data-testid="storyline-input" />
+          </label>
 
-        <label
-          htmlFor="rating-input"
-          data-testid="rating-input-label"
-          name="rating"
-          value={this.state.rating}
-          onChange={this.handleChange}
-        > Avaliação
-          <textarea data-testid="rating-input" />
-        </label>
+          <label
+            htmlFor="rating-input"
+            data-testid="rating-input-label"
+            name="rating"
+            value={this.state.rating}
+            onChange={this.handleChange}
+          > Avaliação
+            <textarea data-testid="rating-input" />
+          </label>
 
-        <label
-          htmlFor="genre-input"
-          data-testid="genre-input-label"
-          name="genre"
-          value={this.state.genre}
-          onChange={this.handleChange}
-        > Gênero
-          <select data-testid="genre-input">
-            <option data-testid="genre-option" value="action">Ação</option>
-            <option data-testid="genre-option" value="comedy">Comédia</option>
-            <option data-testid="genre-option" value="thriller">Suspense</option>
-          </select>
-        </label>
+          <label
+            htmlFor="genre-input"
+            data-testid="genre-input-label"
+            name="genre"
+            value={this.state.genre}
+            onChange={this.handleChange}
+          > Gênero
+            <select data-testid="genre-input">
+              <option data-testid="genre-option" value="action">Ação</option>
+              <option data-testid="genre-option" value="comedy">Comédia</option>
+              <option data-testid="genre-option" value="thriller">Suspense</option>
+            </select>
+          </label>
+        </form>
 
-      </form>
+        <button
+          type="submit"
+          data-testid="send-button"
+          onClick={this.addOneMovie}
+        >
+        Adicionar filme
+        </button>
+      </div>
+
     );
   }
 }
