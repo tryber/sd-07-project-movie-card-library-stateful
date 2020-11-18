e6b6be716a779f1b493de6d59a13ec3a200ac545
Lucas Andrade <lafdesouza2002@outlook.com>
2020-11-18 00:06:28 -0300

    Corrigindo Proptypes

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -15,6 +15,6 @@ class MovieList extends React.Component {
   }
 }
 
-MovieList.propTypes = { movies: PropTypes.shape().isRequired };
+MovieList.propTypes = { movies: PropTypes.arrayOf(PropTypes.object).isRequired };
 
 export default MovieList;
