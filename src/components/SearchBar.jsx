3f53c46afb692f525ab2ba2387f295601b72d155
Camila Valentim <camilaifpb@hotmail.com>
2020-11-18 15:34:07 -0300

    code climate 0.8

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 class SearchBar extends React.Component {
   render() {
     return (
-        <form data-testid="search-bar-form" />
+      <form data-testid="search-bar-form" />
     );
   }
 }
