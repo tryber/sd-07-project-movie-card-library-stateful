b98658c13719ddcbb7741de9360747b821f29627
Camila Valentim <camilaifpb@hotmail.com>
2020-11-18 15:01:23 -0300

    segundo commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './App.css';
-import data from './data';
-
+import movies from './data';
 import Header from './components/Header';
 import MovieLibrary from './components/MovieLibrary';
 
 class App extends React.Component {
   render() {
-  return (
-    <div className="App">
-      <Header />
-      <MovieLibrary movies={data}/>
-    </div>
-  )};
+    return (
+      <div className="App">
+        <Header />
+        <MovieLibrary movies={movies}/>
+      </div>
+    );
+  }
 }
 
 export default App;
