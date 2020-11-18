a9585d0061a58365dcfb842eb6d4f7f747b0e4c9
Camila Valentim <camilaifpb@hotmail.com>
2020-11-18 16:00:43 -0300

    code climate 13

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,9 +5,9 @@ class SearchBar extends React.Component {
   render() {
     return (
       <form data-testid="search-bar-form">
-        <label data-testid="text-input-label" for="texto">
+        <label htmlFor="for" data-testid="text-input-label">
         Inclui o texto:
-          <input data-testid="text-input" id="texto" type="text" onChange={this.onSearchTextChange} />
+          <input data-testid="text-input" id="for" type="text" onChange={this.onSearchTextChange}/>
         </label>
       </form>
     );
