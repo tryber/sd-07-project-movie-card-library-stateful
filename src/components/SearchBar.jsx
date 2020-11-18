130eebeb7156c3880cd464912981af3f26d18108
Eriquy Jhordan <eriquy@gmail.com>
2020-11-18 13:20:28 -0300

    iniciando o projeto. :rocket:

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,12 @@
 // implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <h1>searchBar</h1>
+    );
+  }
+}
+
+export default SearchBar;
