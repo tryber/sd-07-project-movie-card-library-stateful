111e9d8bb926c51476c55dc8ab1f27c432f12de2
Fabio Camargos <fabiocmmcm@gmail.com>
2020-11-18 21:18:28 -0300

    App. js add mi=ovie library and header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './App.css';
+import movies from './data.js';
+import Header from './components/Header';
+import MovieLibrary from './components/MovieLibrary';
 
 import Header from './components/Header';
 
@@ -7,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <MovieLibrary movies={movies} />
     </div>
   );
 }
