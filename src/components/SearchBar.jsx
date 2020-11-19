8b3ca535a8d3bcfc166cb6f857da40e023a07950
gustasaideles <gustasaideles.gmail.com>
2020-11-19 12:32:38 -0300

    SearchBar feito, falta as func

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,47 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+
     return (
-      <div> Algo pra render</div>
+      <form data-testid="search-bar-form">
+        <label htmlFor="text-input" data-testid="text-input-label">
+          Inclui o texto:
+          <input type="text" value={searchText} onChange={onSearchTextChange} data-testid="text-input" />
+        </label>
+        <label htmlFor="fav-checkbox" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} data-testid="checkbox-input" />
+        </label>
+        <label htmlFor="genre-select" data-testid="select-input-label">
+          Filtrar por gênero
+          <select id="genre-select" value={selectedGenre} onChange={onSelectedGenreChange} data-testid="select-input">
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
+        </label>
+      </form>
     );
   }
 }
 
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+};
 export default SearchBar;
