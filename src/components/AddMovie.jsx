11e276782e42460a7f12b923186c6a648d5b735a
IzeldaValeriano <zel.valeriano@gmail.com>
2020-11-19 18:20:00 -0300

    Inclusão de PropTypes

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {}
+    }
+    render(){
+        return (
+            <div> Olá mundo </div>
+        );
+    }
+}
+
+AddMovie.propTypes = { searchText: PropTypes.string.isRequired, 
+onSearchTextChange : PropTypes.func.isRequired,
+bookmarkedOnly : PropTypes.bool.isRequired,
+onBookmarkedChange: PropTypes.func.isRequired,
+selectedGenre : PropTypes.string.isRequired,
+onSelectedGenreChange: PropTypes.func.isRequired
+};
+
+export default AddMovie;
