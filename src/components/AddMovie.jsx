c94844b2dd98a7e98d7cddd340462b467d37cc87
luc-zago <luc.zago@hotmail.com>
2020-11-18 22:47:41 -0300

    Adequações ao CC

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -44,34 +44,84 @@ class AddMovie extends Component {
         <fieldset>
           <div>
             <label htmlFor="title" data-testid="title-input-label">Título</label>
-            <input name="title" data-testid="title-input" type="text" value={this.state.title} onChange={this.handleChange} placeholder="Digite o título do filme" maxLength="40" pattern="[A-zÀ-ÿ ]{1,}" required="required"></input>
+            <input
+            name="title"
+            data-testid="title-input"
+            type="text"
+            value={this.state.title}
+            onChange={this.handleChange}
+            placeholder="Digite o título do filme"
+            maxLength="40"
+            pattern="[A-zÀ-ÿ ]{1,}"
+            required="required" />
           </div>
           <div>
             <label htmlFor="subtitle" data-testid="subtitle-input-label">Subtítulo</label>
-            <input name="subtitle" data-testid="subtitle-input" type="text" value={this.state.subtitle} onChange={this.handleChange} placeholder="Digite o subtítulo do filme" maxLength="100" patern="[A-zÀ-ÿ ]{1,}" required="required"></input>
+            <input
+            name="subtitle"
+            data-testid="subtitle-input"
+            type="text"
+            value={this.state.subtitle}
+            onChange={this.handleChange}
+            placeholder="Digite o subtítulo do filme"
+            maxLength="100"
+            patern="[A-zÀ-ÿ ]{1,}"
+            required="required" />
           </div>
           <div>
             <label htmlFor="image" data-testid="image-input-label">Imagem</label>
-            <input name="imagePath" data-testid="image-input" type="text" value={this.state.imagePath} onChange={this.handleChange} placeholder="Digite o link da imagem do filme" required="required"></input>
+            <input
+            name="imagePath"
+            data-testid="image-input"
+            type="text"
+            value={this.state.imagePath}
+            onChange={this.handleChange}
+            placeholder="Digite o link da imagem do filme"
+            required="required" />
           </div>
           <div>
             <label htmlFor="storyline" data-testid="storyline-input-label">Sinopse</label>
-            <textarea name="storyline" data-testid="storyline-input" value={this.state.storyline} onChange={this.handleChange} placeholder="Escreva a sinopse do filme" maxLength="3000" cols="50" rows="20" pattern="[A-zÀ-ÿ0-9, ]{1,}" required="required"></textarea>
+            <textarea
+            name="storyline"
+            data-testid="storyline-input"
+            value={this.state.storyline}
+            onChange={this.handleChange}
+            placeholder="Escreva a sinopse do filme"
+            maxLength="3000"
+            cols="50"
+            rows="20"
+            pattern="[A-zÀ-ÿ0-9, ]{1,}"
+            required="required" />
           </div>
           <div>
             <label htmlFor="rating" data-testid="rating-input-label">Avaliação</label>
-            <input name="rating" data-testid="rating-input" type="number" step="0.1" min="0" max="5" value={this.state.rating} onChange={this.handleChange} placeholder="Digite a avaliação do filme" required="required"></input>
+            <input
+            name="rating"
+            data-testid="rating-input"
+            type="number"
+            step="0.1"
+            min="0"
+            max="5"
+            value={this.state.rating}
+            onChange={this.handleChange}
+            placeholder="Digite a avaliação do filme"
+            required="required" />
           </div>
           <div>
             <label htmlFor="genre" data-testid="genre-input-label">Gênero</label>
             <select name="genre" data-testid="genre-input" value={this.state.genre} onChange={this.handleChange} required="required">
-            <option data-testid="genre-option" value="action">Ação</option>
-            <option data-testid="genre-option" value="comedy">Comédia</option>
-            <option data-testid="genre-option" value="thriller">Suspense</option>
+              <option data-testid="genre-option" value="action">Ação</option>
+              <option data-testid="genre-option" value="comedy">Comédia</option>
+              <option data-testid="genre-option" value="thriller">Suspense</option>
             </select>
           </div>
           <div>
-            <button name="send" data-testid="send-button" value="1" type="submit" onClick={this.preventEvent}>Adicionar filme</button>
+            <button
+            name="send"
+            data-testid="send-button"
+            value="1"
+            type="submit"
+            onClick={this.preventEvent}>Adicionar filme</button>
           </div>
         </fieldset>
       </form>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -18,25 +18,38 @@ class SearchBar extends Component {
           <div>
             <label htmlFor="searchText" data-testid="text-input-label">
                 Inclui o texto</label>
-              <input name="searchText" data-testid="text-input" type="text" value={searchText} onChange={onSearchTextChange} pattern="[A-zÀ-ÿ ]{1,}">
-              </input>
+            <input
+            name="searchText"
+            data-testid="text-input"
+            type="text"
+            value={searchText}
+            onChange={onSearchTextChange}
+            pattern="[A-zÀ-ÿ ]{1,}" />
           </div>
           <div>
             <label htmlFor="checkbox" data-testid="checkbox-input-label">
                 Mostrar somente favoritos</label>
-              <input name="bookmarkedOnly" data-testid="checkbox-input" type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange}>
-              </input>
+            <input
+            name="bookmarkedOnly"
+            data-testid="checkbox-input"
+            type="checkbox"
+            checked={bookmarkedOnly}
+            onChange={onBookmarkedChange} />
           </div>
           <div>
             <label htmlFor="selectedGenre" data-testid="select-input-label">
                 Filtrar por gênero</label>
-              <select name="selectedGenre" data-testid="select-input" value={selectedGenre} onChange={onSelectedGenreChange}>
-                <option data-testid="select-option" value="">Todos</option>
-                <option data-testid="select-option" value="action">Ação</option>
-                <option data-testid="select-option" value="comedy">Comédia</option>
-                <option data-testid="select-option" value="thriller">Suspense</option>
-              </select>
-            </div>
+            <select
+            name="selectedGenre"
+            data-testid="select-input"
+            value={selectedGenre}
+            onChange={onSelectedGenreChange}>
+              <option data-testid="select-option" value="">Todos</option>
+              <option data-testid="select-option" value="action">Ação</option>
+              <option data-testid="select-option" value="comedy">Comédia</option>
+              <option data-testid="select-option" value="thriller">Suspense</option>
+            </select>
+          </div>
         </fieldset>
       </form>
     );
