bb4dff44557bf9d67a4d4b3ecd4498fa20e63fea
Arnaelcio <arnaelcio.gomes@gmail.com>
2020-11-20 12:32:50 -0300

    organizando validação de props

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -21,6 +21,14 @@ class MovieCard extends React.Component {
   }
 }
 
-MovieCard.propTypes = { movie: PropTypes.arrayOf(PropTypes.object).isRequired };
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;
