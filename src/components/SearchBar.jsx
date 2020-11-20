c7c253a6414b34363b6105c1bf75a60d7ec1382b
Leonardo <lecastro16@gmail.com>
2020-11-20 15:56:41 -0300

    montando formulario v0

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,7 +4,7 @@ import React from "react";
 class SearchBar extends React.Component {
   constructor(props) {
     super(props);
-    this.handleClick=this.handleClick.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick() {
@@ -14,8 +14,31 @@ class SearchBar extends React.Component {
   render() {
     return (
       <div>
-        <h1 className="search-bar">Searching</h1>
+        <h1 className="search">Searching</h1>
         <button onClick={this.handleClick}>Meu Botao</button>
+        <form className="form">
+          <div>
+            <label for="includes-text">Inclui o Texto:</label>
+            <br></br>
+            <input type="text" id="includes-text" />
+          </div>
+          <div>
+            <br></br>
+            <input type="checkbox"></input>
+            <label>Mostrar somente favoritos</label>
+          </div>
+          <div>
+            <label>
+              Filtrar por gênero:
+              <select name="favorite-genre">
+                <option value="all">Todos</option>
+                <option value="action">Ação</option>
+                <option value="fantasy">Fantasia</option>
+                <option value="comedy">Comédia</option>
+              </select>
+            </label>
+          </div>
+        </form>
       </div>
     );
   }
