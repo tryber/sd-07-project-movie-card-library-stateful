531ddf469076b8adb7ca05bd3f5a8e9502860835
Camila Valentim <camilaifpb@hotmail.com>
2020-11-20 08:17:00 -0300

    cc identaçoes

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -21,65 +21,65 @@ class AddMovie extends React.Component {
     const value = target.type === 'option' ? target.checked : target.value;
 
     this.setState({
-    [name]: value,
+      [name]: value,
     });
   }
   render() {
     return (
       <form>
-          <label htmlFor="titulo" data-testid="title-input-label">Titulo
+        <label htmlFor="titulo" data-testid="title-input-label">Titulo
             <input
-                  id="titulo"
-                  type="text"
-                  data-testid="title-input"
-                  value={this.state.title}  
+              id="titulo"
+              type="text"
+              data-testid="title-input"
+              value={this.state.title}  
             />
-          </label>
+        </label>
 
-          <label htmlFor="subtitulo" data-testid="subtitle-input-label">Subtítulo
+        <label htmlFor="subtitulo" data-testid="subtitle-input-label">Subtítulo
            <input 
-                  id="subtitulo"
-                  type="text"
-                  data-testid="subtitle-input"
-                  value={this.state.subtitle}
+              id="subtitulo"
+              type="text"
+              data-testid="subtitle-input"
+              value={this.state.subtitle}
             />
-          </label>
+        </label>
 
-            <label htmlFor="photo" data-testid="image-input-label">Imagem
-                <input 
-                    id="photo"
-                    type="text"
-                    data-testid="image-input"
-                    value={this.state.imagePath}
-                />
-            </label>
+        <label htmlFor="photo" data-testid="image-input-label">Imagem
+            <input 
+              id="photo"
+              type="text"
+              data-testid="image-input"
+              value={this.state.imagePath}
+            />
+        </label>
 
-            <label htmlFor="sinopse" data-testid="storyline-input-label">Sinopse
-                <input
-                    id="sinopse"
-                    data-testid="storyline-input" 
-                    type="textarea" 
-                    value={this.state.storyline}
-                />
-            </label>
+        <label htmlFor="sinopse" data-testid="storyline-input-label">Sinopse
+            <input
+              id="sinopse"
+              data-testid="storyline-input" 
+              type="textarea" 
+              value={this.state.storyline}
+            />
+        </label>
 
-            <label htmlFor="avaliacao" data-testid="rating-input-label">Avaliação
-                <input
-                    id="avaliaçao"
-                    data-testid="rating-input" 
-                    type="number" 
-                    value={this.state.rating} 
-                />
-            </label>
+        <label htmlFor="avaliacao" data-testid="rating-input-label">Avaliação
+            <input
+              id="avaliaçao"
+              data-testid="rating-input" 
+              type="number" 
+              value={this.state.rating} 
+            />
+        </label>
 
-            <label htmlFor="genero" data-testid="genre-input-label">Gênero</label>
-            <select value={this.state.genre}>
-                <option data-testid="genre-option" value="action">Ação</option>
-                <option data-testid="genre-option" value="comedy">Comédia</option>
-                <option data-testid="genre-option" value="thriller">Suspense</option>
-            </select>
-            <button data-testid="send-button">Adicionar filme</button>
-        </form>
+        <label htmlFor="genero" data-testid="genre-input-label">Gênero</label>
+        <select value={this.state.genre}>
+            <option data-testid="genre-option" value="action">Ação</option>
+            <option data-testid="genre-option" value="comedy">Comédia</option>
+            <option data-testid="genre-option" value="thriller">Suspense</option>
+        </select>
+        <button data-testid="send-button">Adicionar filme</button>
+    </form>
     )
    }   
 }
