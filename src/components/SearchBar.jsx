e4efb41080ac7ffb05a00f0fbd06703493c20e50
Leonardo <lecastro16@gmail.com>
2020-11-20 17:15:12 -0300

    requisito 1 ao 5 v1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -9,7 +9,7 @@ class SearchBar extends React.Component {
 
   handleClick() {
     console.log(this);
-    console.log('clicou!');
+    console.log("clicou!");
   }
   render() {
     const {
@@ -27,21 +27,50 @@ class SearchBar extends React.Component {
         <button onClick={this.handleClick}>Meu Botao</button>
         <form data-testid="search-bar-form">
           <div>
-            <label htmlFor="includes-text" data-testid="text-input-label">Inclui o texto:</label>
-            <input type="text" id="includes-text" value={searchText} onChange={onSearchTextChange} data-testid="text-input" />
+            <label htmlFor="includes-text" data-testid="text-input-label">
+              Inclui o texto:
+            </label>
+            <input
+              type="text"
+              id="includes-text"
+              value={searchText}
+              onChange={onSearchTextChange}
+              data-testid="text-input"
+            />
           </div>
           <div>
-            <input type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} name="show-favorites" data-testid="checkbox-input" />
-            <label htmlFor="show-favorites" data-testid="checkbox-input-label">Mostrar somente favoritos</label>
+            <input
+              type="checkbox"
+              checked={bookmarkedOnly}
+              onChange={onBookmarkedChange}
+              name="show-favorites"
+              data-testid="checkbox-input"
+            />
+            <label htmlFor="show-favorites" data-testid="checkbox-input-label">
+              Mostrar somente favoritos
+            </label>
           </div>
           <div>
             <label htmlFor="filter-by-genre" data-testid="select-input-label">
-            Filtrar por gênero
-              <select name="favorite-genre" data-testid="select-input" value={selectedGenre} onChange={onSelectedGenreChange}>
-                <option value="" data-testid="select-option">Todos</option>
-                <option value="action" data-testid="select-option">Ação</option>
-                <option value="comedy" data-testid="select-option">Comédia</option>
-                <option value="thriller" data-testid="select-option">Fantasia</option>
+              Filtrar por gênero
+              <select
+                name="favorite-genre"
+                data-testid="select-input"
+                value={selectedGenre}
+                onChange={onSelectedGenreChange}
+              >
+                <option value="" data-testid="select-option">
+                  Todos
+                </option>
+                <option value="action" data-testid="select-option">
+                  Ação
+                </option>
+                <option value="comedy" data-testid="select-option">
+                  Comédia
+                </option>
+                <option value="thriller" data-testid="select-option">
+                  Fantasia
+                </option>
               </select>
             </label>
           </div>
