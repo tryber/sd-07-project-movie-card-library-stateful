b0a09b8c93b46f60d5cd3eaa632b82966429993e
Leonardo <lecastro16@gmail.com>
2020-11-20 19:05:20 -0300

    construindo o addMovie v0

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import SearchBar from './components/SearchBar';
 import movies from './data';
 import Header from './components/Header';
+import AddMovie from './components/AddMovie';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <SearchBar movies={movies} />
+      <AddMovie />
     </div>
   );
 }
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,47 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      subtitle: "",
+      title: "",
+      imagePath: "",
+      storyline: "",
+      rating: 0,
+      genre: "action",
+    };
+  }
+
+  handleChange({target}) {
+    console.log(target.value)
+    const {name, value} = target;
+    this.setState({ [name]: value });
+  }
+
+  render() {
+    const { onClick } = this.props;
+
+    return (
+      <form>
+        <div>
+          <label htmlFor="title" data-testid="title-input-label">
+            Título
+          </label>
+          <input
+            type="text"
+            data-testid="title-input"
+            value={this.state.title}
+            onChange={this.handleChange}
+            name="title"
+          ></input>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
