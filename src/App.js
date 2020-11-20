80827bc5e3527f624e166ef04b0adc62459f3139
Camila Valentim <camilaifpb@hotmail.com>
2020-11-20 10:03:10 -0300

    code climate

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ class App extends React.Component {
           selectedGenre={this.selectedGenre}
           onSelectedGenreChange={this.onSelectedGenreChange}
         />
-        <AddMovie onClick={this.buttonClick}/>
+        <AddMovie onClick={this.buttonClick} />
       </div>
     );
   }
