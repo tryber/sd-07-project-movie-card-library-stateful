0233a9a68a4d65ab630c928c8a2f6c1decc079a9
Brune Miguel <bstfreire@gmail.com>
2020-11-21 01:04:33 -0300

    Começando requisito MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,53 @@
-// implement MovieLibrary component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import AddMovie  from './AddMovie';
+
+class MovieLibrary extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchText:'',
+      bookmarkedOnly: false,
+      selectedGenre:'',
+      movies: this.props.movies,
+    };
+    this.onSearchText = this.onSearchText.bind(this);
+    this.onBookmarkedOnly = this.onBookmarkedOnly.bind(this);
+    this.onSelectedGenre = this.onSelectedGenre.bind(this);
+  }
+
+  onSearchText(event) {
+    this.setState({ searchText: event.target.value });
+  }
+
+  onBookmarkedOnly(event) {
+    this.setState({ bookmarkedOnly: event.target.value });
+  }
+
+  onSelectedGenre(event) {
+    this.setState({ onSelectedGenre: event.target.value });
+  }
+
+  render () {
+    return (
+      <div>
+        <SearchBar
+        searchText={this.state.searchText}
+        onSearchText={this.onSearchText}
+        bookmarkedOnly={this.state.searchText}
+        onBookmarkedOnly={this.onBookmarkedOnly}
+        selectedGenre={this.state.selectedGenre}
+        onSelectedGenre={this.onSelectedGenre}
+        />
+        <MovieList />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+
+export default MovieLibrary;
