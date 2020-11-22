7b2d63e565837b8498a23861641281daf42c098e
murilopereira006 <muriloedu006@gmail.com>
2020-11-22 19:55:27 -0300

    filter movies and addmoivies

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -15,22 +15,54 @@ class MovieLibrary extends Component {
     };
 
     this.onChange = this.onChange.bind(this);
+    this.filterMovies = this.filterMovies.bind(this);
+    this.addNewMovies = this.addNewMovies.bind(this);
   }
 
   onChange(event) {
     this.setState({ [event.target.name]: event.target.value });
   }
 
+  filterMovies() {
+    const { movies } = this.props;
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+
+    let filteredMovies = movies.filter(
+      (movie) =>
+        movie.title.includes(searchText) ||
+        movie.subtitle.includes(searchText) ||
+        movie.storyline.includes(searchText),
+      );
+    if (bookmarkedOnly) {
+      filteredMovies = filteredMovies.filter((movie) => movie.bookmarked === true);
+    }
+
+    if (selectedGenre) {
+      filteredMovies = filteredMovies.filter((movie) => movie.genre === selectedGenre);
+    }
+
+    return filteredMovies;
+  }
+
+
+  addNewMovies(movie) {
+    this.setState((previous) => ({ movies: [...previous.movies, movie] }));
+  }
+
   render() {
     return(
-      <SearchBar
-        searchText={this.state.searchText}
-        onSearchChange={this.onChange}
-        bookmarkedOnly={this.state.bookmarkedOnly}
-        onBookmarkedChange={this.onChange}
-        selectedGenre={this.state.selectedGenre}
-        onSelectedGenreChange={this.onChange}
-      />
+      <div>
+        <SearchBar
+          searchText={this.searchText}
+          onSearchChange={this.onChange}
+          bookmarkedOnly={this.bookmarkedOnly}
+          onBookmarkedChange={this.onChange}
+          selectedGenre={this.selectedGenre}
+          onSelectedGenreChange={this.onChange}
+        />
+        <MovieList movies={this.filterMovies()} />
+        <AddMovie onClick={this.addNewMovies} />
+      </div>
     );
   }
 }
