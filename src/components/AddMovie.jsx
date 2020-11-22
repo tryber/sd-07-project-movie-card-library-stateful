cf9d22aa9634559bf344fee39caaa2ef77f28cc3
murilopereira006 <muriloedu006@gmail.com>
2020-11-22 20:15:27 -0300

    sera q agr vai?

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -40,27 +40,56 @@ class AddMovie extends React.Component {
       <form data-testid="add-movie-form">
         <label htmlFor="titleText" data-testid="title-input-label">
           Título
-          <input name="titleText" type="text" value={this.title} data-testid="title-input" onChange={this.changingInputsState} />
+          <input
+            name="titleText"
+            type="text" value={this.title}
+            data-testid="title-input"
+            onChange={this.changingInputsState}
+          />
         </label>
         <label htmlFor="subtitleText" data-testid="subtitle-input-label">
           Subtítulo
-          <input name="subtitleText" type="text"  value={this.subtitle} data-testid="subtitle-input" onChange={this.changingInputsState} />
+          <input
+            name="subtitleText"
+            type="text" 
+            value={this.subtitle}
+            data-testid="subtitle-input"
+            onChange={this.changingInputsState}
+          />
         </label>
         <label htmlFor="img" data-testid="image-input-label">
           Imagem
-          <input name="img" type="text" value={this.imagePath} data-testid="image-input" onChange={this.changingInputsState} />
+          <input
+            name="img"
+            type="text"
+            value={this.imagePath}
+            data-testid="image-input"
+            onChange={this.changingInputsState}
+          />
         </label>
         <label htmlFor="storylineTxt" data-testid="storyline-input-label">
           Sinopse
-          <textarea name="storylineTxt" value={this.storyline} data-testid="storyline-input" onChange={this.changingInputsState} />
+          <textarea
+            name="storylineTxt"
+            value={this.storyline}
+            data-testid="storyline-input"
+            onChange={this.changingInputsState}
+          />
         </label>
         <label htmlFor="grade" data-testid="rating-input-label">
           Avaliação
-          <input type="number" name="grade" value={this.rating} data-testid="rating-input" onChange={this.changingInputsState} />
+          <input
+            type="number"
+            name="grade"
+            value={this.rating}
+            data-testid="rating-input"
+            onChange={this.changingInputsState}
+          />
         </label>
         <label htmlFor="genre" data-testid="genre-input-label">
           Gênero
-          <select name="genre" value={this.genre} onChange={this.handleChange} data-testid="genre-input" >
+          <select name="genre" value={this.genre}
+            onChange={this.handleChange} data-testid="genre-input" >
             <option data-testid="genre-option" value="action" >
               Ação
             </option>
