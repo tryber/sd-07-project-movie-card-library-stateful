f577e7acdcf7be7c8fe0ee42acf17b370e9814ff
Diego F Affonso <diegofernandesaffonso@gmail.com>
2020-11-22 21:13:28 -0300

    alteração do id do form mais adição do .bind as funções

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types';
 class AddMovie extends Component {
   constructor(props) {
     super(props);
+    this.chanceStateAddMovie = this.chanceStateAddMovie.bind(this);
+    this.clearAndAddMovieState = this.clearAndAddMovieState.bind(this);
     this.state = {
       subtitle: '',
       title: '',
@@ -12,85 +14,88 @@ class AddMovie extends Component {
       rating: 0,
       genre: 'action',
     };
-    this.chanceStateAddMovie = this.chanceStateAddMovie.bind(this);
-    this.clearAndAddMovieState = this.clearAndAddMovieState.bind(this);
   }
 
   chanceStateAddMovie({ target }) {
     const { name, value } = target;
-    this.setState = { [name]: value };
+    this.setState({ [name]: value });
   }
 
   clearAndAddMovieState() {
     const { onClick } = this.props;
     onClick([this.state]);
-    this.setState = {
+    this.setState({
       subtitle: '',
       title: '',
       imagePath: '',
       storyLine: '',
       rating: 0,
       genre: 'action',
-    };
+    });
   }
 
   render() {
     return (
-      <form>
+      <form data-testid="add-movie-form">
 
-        <label htmlFor={this.state.title} data-testid="title-input-label">
-          Titulo
+        <label htmlFor="title" data-testid="title-input-label">
+          Título
           <input
             data-testid="title-input"
             type="text"
+            name="title"
             value={this.state.title}
             onChange={this.chanceStateAddMovie}
           />
         </label>
 
-        <label htmlFor={this.state.subtitle} data-testid="subtitle-input-label">
-          Subtitulo
+        <label htmlFor="subtitle" data-testid="subtitle-input-label">
+          Subtítulo
           <input
             data-testid="subtitle-input"
             type="text"
+            name="subtitle"
             value={this.state.subtitle}
             onChange={this.chanceStateAddMovie}
           />
         </label>
 
-        <label htmlFor={this.state.imagePath} data-testid="image-input-label">
+        <label htmlFor="imagePath" data-testid="image-input-label">
           Imagem
           <input
             data-testid="image-input"
             type="text"
+            name="imagePath"
             value={this.state.imagePath}
             onChange={this.chanceStateAddMovie}
           />
         </label>
 
-        <label htmlFor={this.state.storyLine} data-testid="storyline-input-label">
+        <label htmlFor="storyLine" data-testid="storyline-input-label">
           Sinopse
           <textarea
             data-testid="storyline-input"
+            name="storyLine"
             value={this.state.storyLine}
             onChange={this.chanceStateAddMovie}
           />
         </label>
 
-        <label htmlFor={this.state.rating} data-testid="rating-input-label">
+        <label htmlFor="rating" data-testid="rating-input-label">
           Avaliação
           <input
             data-testid="rating-input"
             type="number"
+            name="rating"
             value={this.state.rating}
             onChange={this.chanceStateAddMovie}
           />
         </label>
 
-        <label htmlFor={this.state.genre} data-testid="genre-input-label">
+        <label htmlFor="genre" data-testid="genre-input-label">
           Gênero
           <select
-            data-testid="genre-input"
+            data-testid="genre-input" name="genre"
             value={this.state.genre} onChange={this.chanceStateAddMovie}
           >
             <option data-testid="genre-option" value="action">Ação</option>
