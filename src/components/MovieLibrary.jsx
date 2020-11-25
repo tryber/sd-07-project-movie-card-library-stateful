f4ee0f6afd76ba74712949a03fe255efb5ff6054
murilopereira006 <muriloedu006@gmail.com>
2020-11-25 20:04:46 -0300

    nao intendo o problema desse MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
-class MovieLibrary extends Component {
+class MovieLibrary extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
