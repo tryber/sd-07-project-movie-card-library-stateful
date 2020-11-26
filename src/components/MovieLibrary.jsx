e027a03035b133a84438faf0c47d22169bac8374
Andre Menezes <dehhmenezes87@gmail.com>
2020-11-26 11:41:37 -0300

    SearchBar

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,18 @@
-// implement MovieLibrary component here
+import React from 'react';
+
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import movies from '../data';
+
+class MovieLibrary extends React.Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <MovieList movies={movies} />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
