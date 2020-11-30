aae62d3bc61007b1b4fe63f6935e51c911177445
Paulo Henrique <ph.pereira.7@gmail.com>
2020-11-30 03:19:14 -0300

    First commit codeclimate try:2

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
+import PropTypes from 'prop-types';
 
 class MovieLibrary extends Component {
   render() {
@@ -17,8 +17,6 @@ class MovieLibrary extends Component {
   }
 }
 
-MovieLibrary.propTypes = {
-  movies: propTypes.array,
-}
+MovieLibrary.propTypes = { movies: PropTypes.arrayOf(PropTypes.object).isRequired, }
 
 export default MovieLibrary;
