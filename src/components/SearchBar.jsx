50fc29977d76296337d895370ed95973e622b42a
André Ferreira <andrefpires1@gmail.com>
2020-11-30 06:21:35 -0300

    iniciando o projeto: Componente SearchBar | CC2

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,33 +2,33 @@ import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
   render() {
-    // const {
-    //   searchText,
-    //   onSearchTextChange,
-    //   bookmarkedOnly,
-    //   onBookmarkedChange,
-    //   selectedGenre,
-    //   onSelectedGenreChange,
-    // } = this.props;
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+    } = this.props;
 
     return (
       <form data-testid="search-bar-form">
-        <label data-testid="text-input-label">
+        <label for="text-input" data-testid="text-input-label" />
           Inclui o texto:
           <input
             type="text"
             value={searchText}
             onChange={onSearchTextChange}
             data-testid="text-input"
+            name="text-input"
           />
-        </label>
-        <label data-testid="checkbox-input-label">
+        <label for="checkbox-input" data-testid="checkbox-input-label">
           Mostrar somente favoritos
           <input
             type="checkbox"
             checked={bookmarkedOnly}
             onChange={onBookmarkedChange}
             data-testid="checkbox-input"
+            name="checkbox-input"
           />
         </label>
         <label data-testid="select-input-label">
