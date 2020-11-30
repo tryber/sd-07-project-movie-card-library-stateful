0dc9c801076771b44dfc0d4ecee4b753c29a459a
Paulo Henrique <ph.pereira.7@gmail.com>
2020-11-30 03:58:47 -0300

    Form in SearchBar try:1

---
+++ src/components/Forms.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Forms extends Component {
+  render() {
+    return (
+      <div>
+        Forms
+        <form data-testid="search-bar-form"></form>
+      </div>
+    );
+  }
+}
+
+export default Forms;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
+import Forms from './Forms';
 
 class SearchBar extends Component {
   render() {
-    return (<div>SearchBar</div>);
+    return (
+      <div>
+        <Forms />
+      </div>);
   }
 }
 
