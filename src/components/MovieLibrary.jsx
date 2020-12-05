46ba9119ea71b6e729cb6a9b976a30761dec103f
Erlingsjunior <erlingsjunior@gmail.com>
2020-12-05 17:54:42 -0300

    Codeclimate

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
-import PropTypes from 'prop-types';
 
 class MovieLibrary extends React.Component {
   constructor(props) {
