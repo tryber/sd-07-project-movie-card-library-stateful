1d7b2f673ced18898dec6a0e59330eeef57962b4
Erlingsjunior <erlingsjunior@gmail.com>
2020-12-05 15:29:41 -0300

    CRiado MovieLibrary searchbar

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,35 @@
-// implement MovieLibrary component here
+import React from 'react';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
+    };
+  }
+
+  render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+
+    return (
+      <div>
+        <SearchBar
+          searchText={searchText}
+          onChange={(event) => this.setState({ searchText: event.target.value })}
+          bookmarkedOnly={bookmarkedOnly}
+          onChange={(event) => this.setState({ bookmarkedOnly: event.target.value })}
+          selectedGenre={selectedGenre}
+          onChange={(event) => this.setState({ selectedGenre: event.target.value })}
+        />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
