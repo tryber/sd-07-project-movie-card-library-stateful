0bb533e421b8391964ffc9a243226ebd196963a7
Erlingsjunior <erlingsjunior@gmail.com>
2020-12-05 21:00:45 -0300

    Ajustando loucura

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,4 +1,4 @@
-import React, { useImperativeHandle } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
