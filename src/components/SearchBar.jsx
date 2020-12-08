5ad3976214e8eb1f9719ce3fa119be062f547dc7
MarceloBorges <melombm@hotmail.com>
2020-12-07 23:19:00 -0500

    criando componente form

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,20 @@
-// implement SearchBar component here
+import React from 'react';
+
+export default class SearchBar extends React.Component {
+  render() {
+    const { searchText, onSearchTextChange } = this.props
+    return (
+      <div>
+        <form data-testid="search-bar-form">
+
+          <label data-testid="text-input-label">
+            Inclui o texto
+            <input data-testid="text-input" type="text" value={searchText} onChange={onSearchTextChange} />
+          </label>
+
+        </form>
+      </div>
+    );
+  }
+}
+
