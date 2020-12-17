1a8b59aef65c6808cd252becd88ffad57c002229
Andre <dehhmenezes87@gmail.com>
2020-12-17 17:31:01 -0300

    test codeclimate

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,64 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      subtitle: "",
+      title: "",
+      imagePath: "",
+      storyLine: "",
+      rating: 0,
+      genre: "action",
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const value = target.type === "checkbox" ? target.cheked : target.value;
+    this.setState({ [target.id]: value })
+  }
+
+  render() {
+    const { subtitle, title, imagePath, storyLine, rating, genre } = this.state;
+    return (
+      <form>
+        <label data-testid="title-input-label">
+          Título
+          <input
+            id="title"
+            type="text"
+            data-testid="title-input"
+            value={ title }
+            onChange={this.handleChange}
+          />
+        </label>
+        <label data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            id="subtitle"
+            type="text"
+            data-testid="subtitle-input"
+            value={ subtitle }
+            onChange={this.handleChange}
+          />
+        </label>
+        <label data-testid="image-input-label">
+          Imagem
+          <input
+            id="imagePath"
+            type="text"
+            data-testid="image-input"
+            value={ imagePath }
+            onChange={ this.handleChange }
+          />
+        </label>
+        
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -2,14 +2,26 @@ import React from 'react';
 
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
 import movies from '../data';
 
 class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.addMovie = this.addMovie.bind(this);
+  }
+
+  addMovie() {
+    console.log("Função Add Movie");
+  }
+
   render() {
     return (
       <div>
         <SearchBar />
-        <MovieList movies={movies} />
+        <MovieList movies={ movies } />
+        <AddMovie onClick={ this.addMovie } />
       </div>
     );
   }
