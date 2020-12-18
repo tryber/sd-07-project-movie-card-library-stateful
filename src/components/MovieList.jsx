8faf483add57a00dd7ad49a269161866a4c87cdf
IzeldaValeriano <zel.valeriano@gmail.com>
2020-12-18 12:12:53 -0300

    corrigindo cc

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,13 +6,13 @@ class MovieList extends React.Component {
 
   render() {
     const { movies } = this.props;
-    
+
     return (
       <div data-testid="movie-list">
-        {movies.map(movie => <MovieCard key={movie.title} movie={movie} />)}
+        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
       </div>
     );
   }
 }
 
-export default MovieList; 
+export default MovieList;
