496fdd097d31d70b5040e8f1f17073c27b7bf635
adrianoForcellini <adrianoforcellinipedretti@gmail.com>
2020-12-18 23:06:49 -0300

    SearchBarForm - add ultimos data testes

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -35,11 +35,11 @@ class SearchBar extends Component {
         </label>
         <label htmlFor="select-input" data-testid="select-input-label">
         Filtrar por gênero
-          <select value={selectedGenre} onChange={onSelectedGenreChange}>
-            <option value="">Todos</option>
-            <option value="action">Ação</option>
-            <option value="comedy">Comédia</option>
-            <option value="thriller">Suspense</option>
+          <select value={selectedGenre} onChange={onSelectedGenreChange} data-testid="select-input">
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
           </select>
         </label>
       </form>
