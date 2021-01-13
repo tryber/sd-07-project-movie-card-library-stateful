e6abfbbd36980730afc9eaa2def6abaa4bf17823
Jorge Alves <alvesdjorge@gmail.com>
2021-01-13 17:13:22 -0300

    entrega de projeto 100%

---
+++ src/components/AddMovie.jsx
@@ -0,0 +1,134 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends Component {
+  constructor() {
+    super();
+    this.handleState = this.handleState.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  handleState(event) {
+    const { name } = event.target;
+    const value = (name !== 'rating') ? event.target.value : Number(event.target.value);
+    this.setState({ [name]: value });
+  }
+
+  handleClick(callback) {
+    callback(this.state);
+    this.setState({
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <form data-testid="add-movie-form">
+          <label htmlFor="title" data-testid="title-input-label">
+            Título
+          </label>
+          <input
+            type="text"
+            name="title"
+            value={this.state.title}
+            onChange={this.handleState}
+            data-testid="title-input"
+          />
+
+          <label htmlFor="subtitle" data-testid="subtitle-input-label">
+            Subtítulo
+          </label>
+          <input
+            type="text"
+            name="subtitle"
+            value={this.state.subtitle}
+            onChange={this.handleState}
+            data-testid="subtitle-input"
+          />
+
+          <label htmlFor="imagePath" data-testid="image-input-label">
+            Imagem
+          </label>
+          <input
+            type="text"
+            name="imagePath"
+            value={this.state.imagePath}
+            onChange={this.handleState}
+            data-testid="image-input"
+          />
+
+          <label htmlFor="storyline" data-testid="storyline-input-label">
+            Sinopse
+          </label>
+          <textarea
+            type="text-area"
+            name="storyline"
+            value={this.state.storyline}
+            onChange={this.handleState}
+            data-testid="storyline-input"
+          />
+
+          <label htmlFor="rating" data-testid="rating-input-label">
+            Avaliação
+          </label>
+          <input
+            type="number"
+            name="rating"
+            value={this.state.rating}
+            onChange={this.handleState}
+            data-testid="rating-input"
+          />
+
+          <label htmlFor="genre" data-testid="genre-input-label">
+            Gênero
+          </label>
+          <select
+            type=""
+            name="genre"
+            value={this.state.genre}
+            onChange={this.handleState}
+            data-testid="genre-input"
+          >
+            <option value="action" data-testid="genre-option">
+              Ação
+            </option>
+            <option value="comedy" data-testid="genre-option">
+              Comédia
+            </option>
+            <option value="thriller" data-testid="genre-option">
+              Suspense
+            </option>
+          </select>
+          <button
+            type="submit"
+            onClick={(event) => {
+              event.preventDefault();
+              this.handleClick(this.props.onClick);
+            }}
+            data-testid="send-button"
+          >
+            Adicionar filme
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+AddMovie.propTypes = { onClick: PropTypes.func.isRequired };
+
+export default AddMovie;
---
+++ src/components/MovieList.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+
+    return (
+      <div data-testid="movie-list" className="movie-list">
+        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+      </div>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      imagePath: PropTypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.number,
+      genre: PropTypes.string,
+    }),
+  ).isRequired,
+};
+
+export default MovieList;
