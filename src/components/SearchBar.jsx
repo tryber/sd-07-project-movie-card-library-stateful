130624eec054aed85c49936b9c73d665b9a67c3c
lucasokazava <lucasokazava@gmail.com>
2021-01-13 18:31:07 -0300

    Finalizando Input Text do Search Bar e definindo as props

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,39 @@
 import React from 'react';
+import Proptypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
     return (
       <form data-testid="search-bar-form">
-        
+        <label htmlFor="Text" data-testid="text-input-label">
+          Inclui o texto:
+          <input
+          type="text"
+          value={searchText}
+          onChange={onSearchTextChange}
+          data-testid="text-input"
+          />
+        </label>
       </form>
     );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: Proptypes.string.isRequired,
+  onSearchTextChange: Proptypes.func.isRequired,
+  bookmarkedOnly: Proptypes.bool.isRequired,
+  onBookmarkedChange: Proptypes.func.isRequired,
+  selectedGenre: Proptypes.string.isRequired,
+  onSelectedGenreChange: Proptypes.func.isRequired,
+};
 
 export default SearchBar;
