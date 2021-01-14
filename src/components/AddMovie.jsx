cecc66e4a8f138853b403dade0b1652d496631aa
lucasokazava <lucasokazava@gmail.com>
2021-01-13 23:38:50 -0300

    Criando o componentee AddMovie && implementando um input de titulo

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,41 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({title: event.target.value});
+  }
+
+  render() {
+    const { onClick } = this.props;
+    return (
+      <form data-testid="add-movie-form">
+        <label htmlFor="Text" data-testid="title-input-label">
+          Título
+          <input
+          type="text"
+          value={this.state.title}
+          onChange={this.handleChange}
+          data-testid="title-input"
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
