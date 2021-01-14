5b4d408703746da56b3bd32523333243b56e399b
lucasokazava <lucasokazava@gmail.com>
2021-01-14 03:43:28 -0300

    Criando o componente MovieLibrary && criando os estados && renderizando o SearchBar

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,28 @@
-// implement MovieLibrary component here
+import React from 'react';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
+    };
+  }
+
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        <SearchBar
+          searchText={this.props.searchText}
+        />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
